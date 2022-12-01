c539b56f643030bc010ee4c5835826fedc59dfaf
Christian Gorenflo <christian@axelar.network>
2022-12-01 03:42:26 -0500

    fix test

--- tests/report.rs
+++ tests/report.rs
@@ -1,6 +1,7 @@
 use error_stack::Report;
 use sentinel::report::{Error, LoggableError};
 
+// Do not move this test or the location field checks break
 #[test]
 fn correct_error_log() {
     let report = Report::new(Error::new("error1".to_string()))
@@ -23,15 +24,15 @@ fn correct_error_log() {
     let expected_err = LoggableError {
         msg: "error3".to_string(),
         attachments: vec!["opaque attachment".to_string()],
-        location: "tests/report.rs:11:10".to_string(),
+        location: "tests/report.rs:12:10".to_string(),
         cause: Some(Box::new(LoggableError {
             msg: "error2".to_string(),
             attachments: vec!["test1".to_string(), "test2".to_string()],
-            location: "tests/report.rs:8:10".to_string(),
+            location: "tests/report.rs:9:10".to_string(),
             cause: Some(Box::new(LoggableError {
                 msg: "error1".to_string(),
                 attachments: vec!["foo1".to_string()],
-                location: "tests/report.rs:6:18".to_string(),
+                location: "tests/report.rs:7:18".to_string(),
                 cause: None,
                 backtrace: None,
             })),
