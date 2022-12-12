dd81ecac058db4f79e8e5d84f8d64990296c86ba
Edwin Guajardo <edwin@axelar.network>
2022-12-12 10:15:51 -0600

    feat(ConnectionRouter): Added connection router contract shell

---
+++ contracts/connection-router/src/contract.rs
@@ -0,0 +1,48 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::{entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+// use cw2::set_contract_version;
+
+use crate::error::ContractError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+/*
+// version info for migration info
+const CONTRACT_NAME: &str = "crates.io:service-registry";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+*/
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    unimplemented!();
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    todo!();
+}
+
+pub mod execute {
+    use super::*;
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
+    todo!();
+}
+
+pub mod query {
+    use super::*;
+}
+
+#[cfg(test)]
+mod tests {}
