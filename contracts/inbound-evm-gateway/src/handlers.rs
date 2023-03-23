bf4ea28a543f004ec879e7372c2815393ba35cbe
Edwin Guajardo <edwin@axelar.network>
2023-03-23 15:15:29 -0600

    chore: refactor handlers

--- contracts/inbound-evm-gateway/src/handlers.rs
+++ contracts/inbound-evm-gateway/src/handlers.rs
@@ -4,38 +4,28 @@ use cosmwasm_std::{from_binary, Event};
 use crate::msg::ActionMessage;
 
 pub fn pending_poll_handler(poll: &Poll, source_chain_name: &str) -> Event {
-    let ActionMessage::ConfirmGatewayTxs {
-        from_nonce,
-        to_nonce,
-    } = from_binary(&poll.message).unwrap();
-
-    Event::new("PollExpired")
-        .add_attribute("chain", source_chain_name)
-        .add_attribute("from_nonce", from_nonce)
-        .add_attribute("to_nonce", to_nonce)
-        .add_attribute("poll_id", poll.id)
+    // TODO: penalize non-voters
+    build_event("PollExpired", poll, source_chain_name)
 }
 
 pub fn failed_poll_handler(poll: &Poll, source_chain_name: &str) -> Event {
-    let ActionMessage::ConfirmGatewayTxs {
-        from_nonce,
-        to_nonce,
-    } = from_binary(&poll.message).unwrap();
-
-    Event::new("PollFailed")
-        .add_attribute("chain", source_chain_name)
-        .add_attribute("from_nonce", from_nonce)
-        .add_attribute("to_nonce", to_nonce)
-        .add_attribute("poll_id", poll.id)
+    build_event("PollFailed", poll, source_chain_name)
 }
 
 pub fn completed_poll_handler(poll: &Poll, source_chain_name: &str) -> Event {
+    // TODO: rewards
+
+    // TODO: message for router
+    build_event("PollCompleted", poll, source_chain_name)
+}
+
+fn build_event(event_type: &str, poll: &Poll, source_chain_name: &str) -> Event {
     let ActionMessage::ConfirmGatewayTxs {
         from_nonce,
         to_nonce,
     } = from_binary(&poll.message).unwrap();
 
-    Event::new("PollCompleted")
+    Event::new(event_type)
         .add_attribute("chain", source_chain_name)
         .add_attribute("from_nonce", from_nonce)
         .add_attribute("to_nonce", to_nonce)
