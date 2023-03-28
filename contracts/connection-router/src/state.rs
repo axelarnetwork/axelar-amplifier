e2e4e173dc2c7d27c5d2b357d7dbbc28e08150b4
CJ Cobb <cj@axelar.network>
2023-03-28 12:31:35 -0400

    fix lint issues

--- contracts/connection-router/src/state.rs
+++ contracts/connection-router/src/state.rs
@@ -1,10 +1,10 @@
 use cosmwasm_std::{Addr, Uint256};
 
 pub struct Message {
-    id: String,
-    destination_addr: Addr,
-    destination_domain: String,
-    source_domain: String,
-    source_addr: Addr,
-    payload_hash: Uint256,
+    _id: String,
+    _destination_addr: Addr,
+    _destination_domain: String,
+    _source_domain: String,
+    _source_addr: Addr,
+    _payload_hash: Uint256,
 }
