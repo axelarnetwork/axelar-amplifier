494c6eb0f695bf66d9bc522a9c68603222f6267e
Edwin Guajardo <edwin@axelar.network>
2023-05-23 08:16:00 -0600

    Added SigningStarted event

---
+++ contracts/batch-signer/src/events.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+use cosmwasm_std::{Event, HexBinary, Uint64};
+use serde_json::to_string;
+
+pub struct SigningStarted {
+    pub proof_id: String,
+    pub sig_key_id: Uint64, // TODO: should this be here? type correct?
+    pub pub_keys: HashMap<String, HexBinary>,
+    pub unsigned_hash: HexBinary,
+}
+
+impl From<SigningStarted> for Event {
+    fn from(other: SigningStarted) -> Self {
+        Event::new("SigningStarted")
+            .add_attribute("proof_id", other.proof_id)
+            .add_attribute("sig_key_id", other.sig_key_id)
+            .add_attribute(
+                "pub_keys",
+                to_string(&other.pub_keys)
+                    .expect("violated invariant: pub_keys are not serializable"),
+            )
+            .add_attribute("unsigned_hash", other.unsigned_hash.to_hex())
+    }
+}
--- contracts/batch-signer/src/lib.rs
+++ contracts/batch-signer/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod contract;
 pub mod error;
+pub mod events;
 pub mod msg;
 pub mod state;
 pub mod types;
