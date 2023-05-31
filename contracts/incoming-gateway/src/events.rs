045d84f0cc9d553bd37c37ae648e5a19c6fcb8da
CJ Cobb <cj@axelar.network>
2023-05-31 16:27:29 -0400

    review

--- contracts/incoming-gateway/src/events.rs
+++ contracts/incoming-gateway/src/events.rs
@@ -11,54 +11,31 @@ impl From<Message> for Vec<Attribute> {
             ("destination_domain", other.destination_domain),
             ("payload_hash", other.payload_hash.to_string()),
         ]
-        .iter()
-        .map(|a| {
-            let attr: Attribute = a.clone().into();
-            attr
-        })
+        .into_iter()
+        .map(Into::into)
         .collect()
     }
 }
 
-pub struct MessageVerified {
-    pub msg: Message,
+pub enum GatewayEvent {
+    MessageVerified { msg: Message},
+    MessageVerificationFailed { msg: Message},
+    MessageExecuted { msg: Message},
+    MessageExecutionFailed { msg: Message},
 }
 
-impl From<MessageVerified> for Event {
-    fn from(other: MessageVerified) -> Self {
-        let attrs: Vec<Attribute> = other.msg.into();
-        Event::new("message_verified").add_attributes(attrs)
-    }
-}
-pub struct MessageVerificationFailed {
-    pub msg: Message,
+fn make_event(event_name: &str, msg: Message) -> Event {
+        let attrs: Vec<Attribute> = msg.into();
+        Event::new(event_name).add_attributes(attrs)
 }
 
-impl From<MessageVerificationFailed> for Event {
-    fn from(other: MessageVerificationFailed) -> Self {
-        let attrs: Vec<Attribute> = other.msg.into();
-        Event::new("message_verification_failed").add_attributes(attrs)
-    }
-}
-
-pub struct MessageExecuted {
-    pub msg: Message,
-}
-
-impl From<MessageExecuted> for Event {
-    fn from(other: MessageExecuted) -> Self {
-        let attrs: Vec<Attribute> = other.msg.into();
-        Event::new("message_executed").add_attributes(attrs)
-    }
-}
-
-pub struct MessageExecutionFailed {
-    pub msg: Message,
-}
-
-impl From<MessageExecutionFailed> for Event {
-    fn from(other: MessageExecutionFailed) -> Self {
-        let attrs: Vec<Attribute> = other.msg.into();
-        Event::new("message_execution_failed").add_attributes(attrs)
+impl From<GatewayEvent> for Event {
+    fn from(other: GatewayEvent) -> Self {
+        match other {
+            GatewayEvent::MessageVerified { msg } => make_event("message_verified", msg),
+            GatewayEvent::MessageExecuted { msg } => make_event("message_verified", msg),
+            GatewayEvent::MessageVerificationFailed { msg } => make_event("message_verification_failed", msg),
+            GatewayEvent::MessageExecutionFailed { msg } => make_event("message_execution_failed", msg),
+        }
     }
 }
