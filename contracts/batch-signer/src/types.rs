dd7468be0d81740d94e4a91ef47c933b20d8b1ce
Edwin Guajardo <edwin@axelar.network>
2023-06-05 13:13:30 -0600

    Removed tx hash and event index from encodings

--- contracts/batch-signer/src/types.rs
+++ contracts/batch-signer/src/types.rs
@@ -1,5 +1,5 @@
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Addr, HexBinary, Uint256, Uint64};
+use cosmwasm_std::{Addr, HexBinary, Uint256};
 use ethabi::ethereum_types;
 use std::{fmt::Display, str::FromStr};
 
@@ -15,43 +15,14 @@ pub struct Message {
     pub source_chain: String,
     pub destination_address: ethereum_types::Address,
     pub payload_hash: KeccackHash,
-    pub source_tx_hash: KeccackHash,
-    pub source_event_index: Uint64,
 }
 
 impl TryFrom<connection_router::types::Message> for Message {
     type Error = ContractError;
 
     fn try_from(msg: connection_router::types::Message) -> Result<Self, Self::Error> {
-        // TODO: confirm message id format
-        // TODO: move all id deconstruction to Message struct impl?
-        let id = msg.id();
-        let id_parts: Vec<&str> = id.split(ID_SEPARATOR).collect();
-        let id_parts: [&str; 2] =
-            id_parts
-                .try_into()
-                .map_err(|_| ContractError::InvalidMessage {
-                    context: "message id is not in the format of {tx_hash}-{event_index}".into(),
-                })?;
-
-        let source_tx_hash =
-            hex::decode(id_parts[0]).map_err(|_| ContractError::InvalidMessage {
-                context: "source_tx_hash is not a valid hex string".into(),
-            })?;
-        let source_tx_hash =
-            source_tx_hash
-                .try_into()
-                .map_err(|_| ContractError::InvalidMessage {
-                    context: "source_tx_hash is not a valid keccak256 hash".into(),
-                })?;
-
-        let source_event_index =
-            Uint64::try_from(id_parts[1]).map_err(|_| ContractError::InvalidMessage {
-                context: "source_event_index is not a valid uint256".into(),
-            })?;
-
         Ok(Message {
-            id,
+            id: msg.id(),
             source_address: msg.source_address,
             source_chain: msg.source_domain.into(),
             destination_address: ethereum_types::Address::from_str(&msg.destination_address)
@@ -63,8 +34,6 @@ impl TryFrom<connection_router::types::Message> for Message {
                     context: "payload_hash is not a valid keccak256 hash".into(),
                 }
             })?,
-            source_tx_hash,
-            source_event_index,
         })
     }
 }
