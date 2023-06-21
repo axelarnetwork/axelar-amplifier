db011d794f3f83e6c05f7967d1606375513005bd
CJ Cobb <cj@axelar.network>
2023-06-21 14:26:31 -0400

    add participants to poll started event

--- contracts/voting-verifier/src/events.rs
+++ contracts/voting-verifier/src/events.rs
@@ -1,15 +1,29 @@
 use connection_router::events::make_message_event;
 use connection_router::state::Message;
-use cosmwasm_std::Event;
+use cosmwasm_std::{Addr, Event};
 
 pub struct PollStarted {
     messages: Vec<Message>,
     poll_id: String,
+    participants: Vec<Addr>,
 }
 
 impl From<PollStarted> for Vec<Event> {
     fn from(other: PollStarted) -> Self {
-        let ev = Event::new("poll_started").add_attribute("poll_id", other.poll_id);
+        let ev = Event::new("poll_started")
+            .add_attribute("poll_id", other.poll_id)
+            .add_attribute(
+                "participants",
+                format!(
+                    "[{}]",
+                    other
+                        .participants
+                        .iter()
+                        .map(|p| p.to_string())
+                        .collect::<Vec<_>>()
+                        .join(",")
+                ),
+            );
         let mut evs: Vec<Event> = other
             .messages
             .into_iter()
