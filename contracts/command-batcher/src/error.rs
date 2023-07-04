794765ff7f98de06675670037e90ff5bfb5d4dc1
Edwin Guajardo <edwin@axelar.network>
2023-07-04 14:25:29 -0600

    Removed unused crate and errors

--- contracts/command-batcher/src/error.rs
+++ contracts/command-batcher/src/error.rs
@@ -1,17 +1,10 @@
 use cosmwasm_std::StdError;
-use cw_utils::ParseReplyError;
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
-
-    #[error(transparent)]
-    ParseReplyError(#[from] ParseReplyError),
-
-    #[error("Invalid reply ID")]
-    InvalidReplyID,
 
     #[error("Message is invalid: {context}")]
     InvalidMessage { context: String },
