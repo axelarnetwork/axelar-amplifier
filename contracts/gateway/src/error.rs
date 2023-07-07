000a93ef4262289f84c957bfaa830267e388dfbd
CJ Cobb <cj@axelar.network>
2023-07-07 13:02:37 -0400

    review

--- contracts/gateway/src/error.rs
+++ contracts/gateway/src/error.rs
@@ -6,19 +6,8 @@ pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
 
-    #[error("domain name is invalid")]
-    InvalidDomainName {},
-
-    #[error("message ID is invalid")]
-    InvalidMessageID {},
-
-    #[error("address of {0} is invalid")]
-    InvalidAddress(String),
-
-    #[error("router error")]
-    RouterError {
-        error: connection_router::error::ContractError,
-    },
+    #[error("{0}")]
+    RouterError(#[from] connection_router::ContractError),
 
     #[error("message not found")]
     MessageNotFound { message_id: String },
@@ -34,15 +23,4 @@ pub enum ContractError {
 
     #[error("batch contains duplicate message ids")]
     DuplicateMessageID {},
-}
-
-impl From<connection_router::ContractError> for ContractError {
-    fn from(value: connection_router::ContractError) -> Self {
-        match value {
-            connection_router::ContractError::InvalidAddress(addr) => Self::InvalidAddress(addr),
-            connection_router::ContractError::InvalidDomainName {} => Self::InvalidDomainName {},
-            connection_router::ContractError::InvalidMessageID {} => Self::InvalidDomainName {},
-            _ => Self::RouterError { error: value },
-        }
-    }
 }
