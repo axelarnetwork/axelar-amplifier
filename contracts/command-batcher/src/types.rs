fa96ecc360c92f1fb5347aa07b20d03b754e7439
Edwin Guajardo <edwin@axelar.network>
2023-07-21 07:53:39 -0600

    Updated Proof struct

--- contracts/command-batcher/src/types.rs
+++ contracts/command-batcher/src/types.rs
@@ -1,5 +1,5 @@
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Addr, HexBinary, Uint256};
+use cosmwasm_std::{HexBinary, Uint256};
 
 #[cw_serde]
 pub struct Data {
@@ -10,9 +10,14 @@ pub struct Data {
 }
 
 #[cw_serde]
+pub struct Operator {
+    pub address: HexBinary,
+    pub weight: Uint256,
+    pub signature: Option<HexBinary>,
+}
+
+#[cw_serde]
 pub struct Proof {
-    pub operators: Vec<Addr>,
-    pub weights: Vec<Uint256>,
+    pub operators: Vec<Operator>,
     pub threshold: Uint256,
-    pub signatures: Vec<HexBinary>,
 }
