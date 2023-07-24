c283e699b49c84533f5468d93241861e2c1959d4
Edwin Guajardo <edwin@axelar.network>
2023-07-24 11:18:05 -0600

    Added get proof query

--- contracts/command-batcher/src/state.rs
+++ contracts/command-batcher/src/state.rs
@@ -1,1 +1,20 @@
+use axelar_wasm_std::Threshold;
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{Addr, Uint256};
+use cw_storage_plus::{Item, Map};
 
+use crate::types::CommandBatch;
+
+#[cw_serde]
+pub struct Config {
+    pub admin: Addr,
+    pub gateway: Addr,
+    pub multisig: Addr,
+    pub service_registry: Addr,
+    pub destination_chain_id: Uint256,
+    pub quorum_threshold: Threshold,
+    pub service_name: String,
+}
+
+pub const CONFIG: Item<Config> = Item::new("config");
+pub const COMMANDS_BATCH: Map<&[u8], CommandBatch> = Map::new("command_batch");
