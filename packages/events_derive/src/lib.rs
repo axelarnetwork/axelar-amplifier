1a19a909f927bf196ff876dc18e6cef12d4e22e9
Christian Gorenflo <christian@axelar.network>
2023-08-19 01:09:05 -0400

    rename

--- packages/events_derive/src/lib.rs
+++ packages/events_derive/src/lib.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 use quote::quote;
 
 #[proc_macro_attribute]
-pub fn derive_tryfrom(arg: TokenStream, input: TokenStream) -> TokenStream {
+pub fn tryfrom(arg: TokenStream, input: TokenStream) -> TokenStream {
     let event_type = syn::parse_macro_input!(arg as syn::LitStr);
     let input = syn::parse_macro_input!(input as syn::ItemStruct);
 
@@ -14,30 +14,30 @@ pub fn derive_tryfrom(arg: TokenStream, input: TokenStream) -> TokenStream {
 
         use error_stack::{IntoReport as _, ResultExt as _};
 
-        impl TryFrom<&tryfrom_utils::Event> for #event_struct {
-            type Error = error_stack::Report<tryfrom_utils::Error>;
+        impl TryFrom<&events::Event> for #event_struct {
+            type Error = error_stack::Report<events::Error>;
 
-            fn try_from(event: &tryfrom_utils::Event) -> core::result::Result<Self, Self::Error> {
+            fn try_from(event: &events::Event) -> core::result::Result<Self, Self::Error> {
                 match event {
-                    tryfrom_utils::Event::Abci { event_type, attributes } if event_type.as_str() == #event_type => {
+                    events::Event::Abci { event_type, attributes } if event_type.as_str() == #event_type => {
                         let event =
                             TestEvent::deserialize(serde::de::value::MapDeserializer::new(attributes.clone().into_iter()))
                                 .into_report()
-                                .change_context(tryfrom_utils::Error::DeserializationFailed(
+                                .change_context(events::Error::DeserializationFailed(
                                     #event_type.to_string(),
                                     #event_struct_name.to_string()),
                                 )?;
                         Ok(event)
                     }
-                    _ => Err(tryfrom_utils::Error::EventTypeMismatch(#event_type.to_string())).into_report(),
+                    _ => Err(events::Error::EventTypeMismatch(#event_type.to_string())).into_report(),
                 }
             }
         }
 
-        impl TryFrom<tryfrom_utils::Event> for TestEvent {
-            type Error = error_stack::Report<tryfrom_utils::Error>;
+        impl TryFrom<events::Event> for TestEvent {
+            type Error = error_stack::Report<events::Error>;
 
-            fn try_from(event: tryfrom_utils::Event) -> core::result::Result<Self, Self::Error> {
+            fn try_from(event: events::Event) -> core::result::Result<Self, Self::Error> {
                 Self::try_from(&event)
             }
         }
--- packages/events_derive/tests/macro.rs
+++ packages/events_derive/tests/macro.rs
@@ -1,10 +1,9 @@
 use error_stack::Result;
 use serde::Deserialize;
-use tryfrom_derive::derive_tryfrom;
 
 #[allow(dead_code)]
 #[derive(Debug, Deserialize)]
-#[derive_tryfrom("test_event")]
+#[events_derive::tryfrom("test_event")]
 struct TestEvent {
     pub number: usize,
     pub text: String,
@@ -15,15 +14,15 @@ fn fail_to_convert_incompatible_event() {
     let mut missing_attributes = serde_json::Map::new();
     missing_attributes.insert("number".to_string(), serde_json::to_value(10).unwrap());
 
-    let incompatible_event = tryfrom_utils::Event::Abci {
+    let incompatible_event = events::Event::Abci {
         event_type: "test_event".to_string(),
         attributes: missing_attributes,
     };
 
-    let res: Result<TestEvent, tryfrom_utils::Error> = incompatible_event.try_into();
+    let res: Result<TestEvent, events::Error> = incompatible_event.try_into();
     assert!(res.is_err_and(|err| matches!(
         err.current_context(),
-        tryfrom_utils::Error::DeserializationFailed(_, _)
+        events::Error::DeserializationFailed(_, _)
     )));
 }
 
@@ -36,16 +35,15 @@ fn fail_to_convert_event_with_type_mismatch() {
         serde_json::to_value("some text").unwrap(),
     );
 
-    let mismatched_event = tryfrom_utils::Event::Abci {
+    let mismatched_event = events::Event::Abci {
         event_type: "some_other_event".to_string(),
         attributes: complete_attributes.clone(),
     };
 
-    let res: Result<TestEvent, tryfrom_utils::Error> = mismatched_event.try_into();
-    assert!(res.is_err_and(|err| matches!(
-        err.current_context(),
-        tryfrom_utils::Error::EventTypeMismatch(_)
-    )));
+    let res: Result<TestEvent, events::Error> = mismatched_event.try_into();
+    assert!(
+        res.is_err_and(|err| matches!(err.current_context(), events::Error::EventTypeMismatch(_)))
+    );
 }
 
 #[test]
@@ -57,11 +55,11 @@ fn convert_matching_event() {
         serde_json::to_value("some text").unwrap(),
     );
 
-    let correct_event = tryfrom_utils::Event::Abci {
+    let correct_event = events::Event::Abci {
         event_type: "test_event".to_string(),
         attributes: complete_attributes,
     };
 
-    let res: Result<TestEvent, tryfrom_utils::Error> = correct_event.try_into();
+    let res: Result<TestEvent, events::Error> = correct_event.try_into();
     assert!(res.is_ok());
 }
