6fedc48f5fafc820b2cca9d0106f1cf873812182
Christian Gorenflo <christian@axelar.network>
2023-08-31 17:46:12 -0400

    fmt

--- packages/axelar-wasm-std/src/lib.rs
+++ packages/axelar-wasm-std/src/lib.rs
@@ -1,15 +1,14 @@
-pub mod counter;
-pub mod flagset;
-mod fn_ext;
-pub mod nonempty;
-pub mod operators;
-
-pub mod snapshot;
-pub mod threshold;
-pub mod voting;
-
 pub use crate::{
     fn_ext::FnExt,
     snapshot::{Participant, Snapshot},
     threshold::Threshold,
 };
+
+pub mod counter;
+pub mod flagset;
+mod fn_ext;
+pub mod nonempty;
+pub mod operators;
+pub mod snapshot;
+pub mod threshold;
+pub mod voting;
