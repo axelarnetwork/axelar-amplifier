ab1a3f304476eb0465cebf2ac1668a46f87e99a9
Christian Gorenflo <christian@axelar.network>
2023-09-09 03:13:39 -0400

    fix: resolve '~' in ampd paths

--- packages/axelar-wasm-std/src/utils.rs
+++ packages/axelar-wasm-std/src/utils.rs
@@ -4,3 +4,37 @@ where
 {
     vec.into_iter().map(f).collect::<Result<Vec<B>, E>>()
 }
+
+pub trait InspectorResult<T, E> {
+    /// This function has a trailing underscore to avoid name collision with the unstable [core::result::Result::inspect](https://doc.rust-lang.org/core/result/enum.Result.html#method.inspect) function.
+    fn inspect_<F>(self, f: F) -> Self
+    where
+        F: FnOnce(&T);
+
+    /// This function has a trailing underscore to avoid name collision with the unstable [core::result::Result::inspect_err](https://doc.rust-lang.org/core/result/enum.Result.html#method.inspect_err) function.
+    fn inspect_err_<F>(self, f: F) -> Self
+    where
+        F: FnOnce(&E);
+}
+
+impl<T, E> InspectorResult<T, E> for Result<T, E> {
+    fn inspect_<F>(self, f: F) -> Self
+    where
+        F: FnOnce(&T),
+    {
+        self.map(|t| {
+            f(&t);
+            t
+        })
+    }
+
+    fn inspect_err_<F>(self, f: F) -> Self
+    where
+        F: FnOnce(&E),
+    {
+        self.map_err(|e| {
+            f(&e);
+            e
+        })
+    }
+}
