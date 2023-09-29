205bd95be998ccc2e6ff9cade955a09bad0eb106
CJ Cobb <cj@axelar.network>
2023-09-29 09:41:37 -0400

    review

--- contracts/rewards/src/msg.rs
+++ contracts/rewards/src/msg.rs
@@ -1,4 +1,4 @@
-use axelar_wasm_std::nonempty;
+use axelar_wasm_std::{nonempty, Threshold};
 use cosmwasm_schema::cw_serde;
 
 #[cw_serde]
@@ -10,47 +10,48 @@ pub struct InstantiateMsg {
 
 #[cw_serde]
 pub struct RewardsParams {
-    // How often rewards are calculated, specified in number of blocks. Participation is calculated over this window. So if epoch is 500
-    // blocks, validators are rewarded for their participation within each 500 block window.
+    /// How often rewards are calculated, specified in number of blocks. Participation is calculated over this window. So if epoch is 500
+    /// blocks, workers are rewarded for their participation within each 500 block window.
     epoch_duration: nonempty::Uint64,
-    // Total length of time over which rewards in a pool are distributed, specified in number of blocks. For example, if pool_duration is
-    // 1 million blocks, and epoch_duration is 500 blocks, every 500 blocks, 1 million / 500 ( = 5,000) tokens are distributed to participating
-    // validators. The tokens to be distributed are split equally amongst the participating validators
+
+    /// Total length of time over which rewards in a pool are distributed, specified in number of blocks. For example, if pool_duration is
+    /// 1 million blocks, and epoch_duration is 500 blocks, and total size of the reward pool is 100k tokens, every 500 blocks,
+    /// 100k * 500 / 1 million (= 50) tokens are distributed to participating workers. The tokens to be distributed are split equally amongst the participating workers
     pool_duration: nonempty::Uint64,
-    // Participation threshold validators must meet to receive rewards in a given epoch, specified as a fraction between 0 and 1. Validators
-    // must participate in at least this fraction of all events in a given epoch to receive rewards. So, if participation_threshold is 9/10,
-    // and there are 100 events in a given epoch, validators must have participated in at least 90 events to receive rewards.
-    // Participation is reset at the beginning of each epoch, so participation in previous epochs does not affect rewards for future epochs.
-    participation_threshold: nonempty::Uint64,
+
+    /// Participation threshold workers must meet to receive rewards in a given epoch, specified as a fraction between 0 (exclusive) and 1 (exclusive). workers
+    /// must participate in at least this fraction of all events in a given epoch to receive rewards. So, if participation_threshold is 9/10,
+    /// and there are 100 events in a given epoch, workers must have participated in at least 90 events to receive rewards.
+    /// Participation is reset at the beginning of each epoch, so participation in previous epochs does not affect rewards for future epochs.
+    participation_threshold: Threshold,
 }
 
 #[cw_serde]
 pub enum ExecuteMsg {
-    // Called each time a new event is started, for which validators are rewarded for participating
-    StartValidatorEvent {
+    /// Called each time a new event is started, for which workers are rewarded for participating
+    StartRewardEvent { event_id: String },
+
+    /// Log a specific worker as participating in a specific event
+    WorkerParticipated {
         event_id: String,
+        worker_address: String,
     },
-    // Log a specific validator as participating in a specific event
-    ValidatorParticipated {
-        event_id: String,
-        validator_address: String,
-    },
-    // Process rewards for the most recent epoch, if not yet processed, and send the required number of tokens to each validator
+
+    /// Process rewards for the most recent epoch, if not yet processed, and send the required number of tokens to each worker
     ProcessRewards {
-        // Address of contract for which to process rewards.
-        // For example, address of a voting verifier instance.
+        /// Address of contract for which to process rewards. For example, address of a voting verifier instance.
         contract_address: String,
     },
-    // Increase the number of AXL tokens
+
+    /// Start a new reward pool for the given address if none exists. Otherwise, add tokens to an existing reward pool.
+    /// Any attached funds a denom matching the rewards denom are added to the pool.
     AddRewards {
-        // Address of contract for which to reward participation.
-        // For example, address of a voting verifier instance. Validators who vote
+        /// Address of contract for which to reward participation. For example, address of a voting verifier instance.
         contract_address: String,
     },
-    // callable only by governance. overwrites currently stored params
-    ModifyParams {
-        params: RewardsParams,
-    },
+
+    /// Overwrites the currently stored params. Callable only by governance.
+    ModifyParams { params: RewardsParams },
 }
 
 #[cw_serde]
