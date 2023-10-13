f8bd79643f01f063bb79ea04e81391b9924ac2f9
Christian Gorenflo <christian@axelar.network>
2023-10-13 16:52:32 -0400

    refactor: extract gateway and router apis into separate packages (#146)

---
+++ packages/connection-router-api/src/msg.rs
@@ -0,0 +1,47 @@
+use crate::primitives::*;
+use cosmwasm_schema::{cw_serde, QueryResponses};
+
+#[cw_serde]
+pub enum ExecuteMsg {
+    /*
+     * Governance Methods
+     * All of the below messages should only be called by governance
+     */
+    // Registers a new chain with the router
+    RegisterChain {
+        chain: ChainName,
+        gateway_address: Address,
+    },
+    // Changes the gateway address associated with a particular chain
+    UpgradeGateway {
+        chain: ChainName,
+        contract_address: Address,
+    },
+
+    /*
+     * Router Admin Methods
+     * All of the below messages should only be called by the router admin
+     */
+    // Freezes a chain, in the specified direction.
+    FreezeChain {
+        chain: ChainName,
+        direction: GatewayDirection,
+    },
+    // Unfreezes a chain, in the specified direction.
+    UnfreezeChain {
+        chain: ChainName,
+        direction: GatewayDirection,
+    },
+
+    /*
+     * Gateway Messages
+     * The below messages can only be called by registered gateways
+     */
+    // Routes a message to all outgoing gateways registered to the destination domain.
+    // Called by an incoming gateway
+    RouteMessages(Vec<Message>),
+}
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {}
