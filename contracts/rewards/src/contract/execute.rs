5cfdfb17450fbd17363cbe5ff79933b3b44af372
CJ Cobb <46455409+cjcobb23@users.noreply.github.com>
2023-10-23 17:21:28 -0400

    feat(rewards): implement add rewards (#162)
    * feat(rewards): implement add rewards

--- contracts/rewards/src/contract/execute.rs
+++ contracts/rewards/src/contract/execute.rs
@@ -6,8 +6,10 @@ use cosmwasm_std::Addr;
 use crate::{
     error::ContractError,
     msg::RewardsParams,
-    state::{Epoch, Store},
+    state::{Epoch, RewardsPool, Store},
 };
+
+use error_stack::Result;
 
 pub struct Contract<S>
 where
@@ -70,24 +72,39 @@ where
         todo!()
     }
 
-    pub fn add_rewards(
-        &mut self,
-        _contract: Addr,
-        _amount: Uint256,
-        _block_height: u64,
-    ) -> Result<(), ContractError> {
-        todo!()
+    pub fn add_rewards(&mut self, contract: Addr, amount: Uint256) -> Result<(), ContractError> {
+        let pool = self.store.load_rewards_pool(contract.clone())?;
+
+        let updated_pool = match pool {
+            Some(pool) => RewardsPool {
+                balance: pool.balance + cosmwasm_std::Uint256::from(amount),
+                ..pool
+            },
+            None => RewardsPool {
+                contract,
+                balance: amount.into(),
+            },
+        };
+
+        self.store.save_rewards_pool(&updated_pool)?;
+
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod test {
+    use std::{
+        collections::HashMap,
+        sync::{Arc, RwLock},
+    };
+
     use axelar_wasm_std::nonempty;
-    use cosmwasm_std::{Uint256, Uint64};
+    use cosmwasm_std::{Addr, Uint256, Uint64};
 
     use crate::{
         msg::RewardsParams,
-        state::{self, Epoch, StoredParams},
+        state::{self, Epoch, RewardsPool, Store, StoredParams},
     };
 
     use super::Contract;
@@ -156,18 +173,126 @@ mod test {
         }
     }
 
-    fn create_contract(stored_params: StoredParams) -> Contract<state::MockStore> {
+    /// Tests that rewards are added correctly to a single contract
+    #[test]
+    fn add_rewards() {
+        let cur_epoch_num = 1u64;
+        let block_height_started = 250u64;
+        let epoch_duration = 100u64;
+        let rewards_store = Arc::new(RwLock::new(HashMap::new()));
+
+        let mut contract = setup_with_stores(
+            cur_epoch_num,
+            block_height_started,
+            epoch_duration,
+            rewards_store,
+        );
+        let worker_contract = Addr::unchecked("some contract");
+        let pool = contract
+            .store
+            .load_rewards_pool(worker_contract.clone())
+            .unwrap();
+        assert!(pool.is_none());
+
+        let initial_amount = Uint256::from(100u128);
+        contract
+            .add_rewards(worker_contract.clone(), initial_amount.try_into().unwrap())
+            .unwrap();
+
+        let pool = contract
+            .store
+            .load_rewards_pool(worker_contract.clone())
+            .unwrap();
+        assert!(pool.is_some());
+        assert_eq!(pool.unwrap().balance, initial_amount);
+
+        let added_amount = Uint256::from(500u128);
+        contract
+            .add_rewards(worker_contract.clone(), added_amount.try_into().unwrap())
+            .unwrap();
+
+        let pool = contract
+            .store
+            .load_rewards_pool(worker_contract)
+            .unwrap()
+            .unwrap();
+        assert_eq!(pool.balance, initial_amount + added_amount);
+    }
+
+    /// Tests that rewards are added correctly with multiple contracts
+    #[test]
+    fn add_rewards_multiple_contracts() {
+        let cur_epoch_num = 1u64;
+        let block_height_started = 250u64;
+        let epoch_duration = 100u64;
+        let rewards_store = Arc::new(RwLock::new(HashMap::new()));
+
+        let mut contract = setup_with_stores(
+            cur_epoch_num,
+            block_height_started,
+            epoch_duration,
+            rewards_store,
+        );
+        // a vector of (worker contract, rewards amounts) pairs
+        let test_data = vec![
+            (Addr::unchecked("contract_1"), vec![100, 200, 50]),
+            (Addr::unchecked("contract_2"), vec![25, 500, 70]),
+            (Addr::unchecked("contract_3"), vec![1000, 500, 2000]),
+        ];
+
+        for (worker_contract, rewards) in &test_data {
+            for amount in rewards {
+                contract
+                    .add_rewards(
+                        worker_contract.clone(),
+                        cosmwasm_std::Uint256::from(*amount as u128)
+                            .try_into()
+                            .unwrap(),
+                    )
+                    .unwrap();
+            }
+        }
+
+        for (worker_contract, rewards) in test_data {
+            let pool = contract
+                .store
+                .load_rewards_pool(worker_contract)
+                .unwrap()
+                .unwrap();
+            assert_eq!(
+                pool.balance,
+                cosmwasm_std::Uint256::from(rewards.iter().sum::<u128>())
+            );
+        }
+    }
+
+    fn create_contract(
+        stored_params: StoredParams,
+        rewards_store: Arc<RwLock<HashMap<Addr, RewardsPool>>>,
+    ) -> Contract<state::MockStore> {
         let mut store = state::MockStore::new();
         store
             .expect_load_params()
             .returning(move || stored_params.clone());
+
+        let rewards_store_cloned = rewards_store.clone();
+        store.expect_load_rewards_pool().returning(move |contract| {
+            let rewards_store = rewards_store_cloned.read().unwrap();
+            Ok(rewards_store.get(&contract).cloned())
+        });
+        store.expect_save_rewards_pool().returning(move |pool| {
+            let mut rewards_store = rewards_store.write().unwrap();
+            rewards_store.insert(pool.contract.clone(), pool.clone());
+            Ok(())
+        });
         Contract { store }
     }
 
-    fn setup(
+    fn setup_with_stores(
         cur_epoch_num: u64,
         block_height_started: u64,
         epoch_duration: u64,
+        rewards_store: Arc<RwLock<HashMap<Addr, RewardsPool>>>,
     ) -> Contract<state::MockStore> {
         let rewards_per_epoch: nonempty::Uint256 = Uint256::from(100u128).try_into().unwrap();
         let current_epoch = Epoch {
@@ -185,6 +310,20 @@ mod test {
             last_updated: current_epoch.clone(),
         };
 
-        create_contract(stored_params.clone())
+        create_contract(stored_params.clone(), rewards_store)
+    }
+
+    fn setup(
+        cur_epoch_num: u64,
+        block_height_started: u64,
+        epoch_duration: u64,
+    ) -> Contract<state::MockStore> {
+        let rewards_store = Arc::new(RwLock::new(HashMap::new()));
+        setup_with_stores(
+            cur_epoch_num,
+            block_height_started,
+            epoch_duration,
+            rewards_store,
+        )
     }
 }
