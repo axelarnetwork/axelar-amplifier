136e97a8293e01623568ed56305e605ab919c55d
Haiyi Zhong <haiyizxx@gmail.com>
2023-10-30 22:23:06 +0800

    feat(service-registry): service and worker query

--- contracts/multisig-prover/src/test/mocks/service_registry.rs
+++ contracts/multisig-prover/src/test/mocks/service_registry.rs
@@ -94,5 +94,7 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
 
             to_binary(&workers)
         }
+        service_registry::msg::QueryMsg::GetService { .. }
+        | service_registry::msg::QueryMsg::GetWorker { .. } => todo!(),
     }
 }
