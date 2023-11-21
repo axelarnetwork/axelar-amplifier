fe2202135556bc9ca48eacd5a671daf2219de2aa
fahimahmedx <f92ahmed@uwaterloo.ca>
2023-11-21 18:18:29 -0500

    Use aliasing for the key of the WORKER_SETS map

--- contracts/multisig/src/state.rs
+++ contracts/multisig/src/state.rs
@@ -56,8 +56,8 @@ pub fn save_signature(
     )
 }
 
-// the map's key is the WorkerSet's id
-pub const WORKER_SETS: Map<&String, WorkerSet> = Map::new("worker_sets");
+type WorkerSetID = String;
+pub const WORKER_SETS: Map<&WorkerSetID, WorkerSet> = Map::new("worker_sets");
 pub fn get_worker_set(
     store: &dyn Storage,
     worker_set_id: &String,
