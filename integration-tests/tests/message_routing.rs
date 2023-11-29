e8520176c6768db2758d46e6bfbe5d98b8600628
CJ Cobb <cj@axelar.network>
2023-11-29 16:57:55 -0500

    review

--- integration-tests/tests/message_routing.rs
+++ integration-tests/tests/message_routing.rs
@@ -7,7 +7,7 @@ mod test_utils;
 /// gateway, votes on the poll, routes the message to the outgoing gateway, triggers signing at the prover
 /// and signs via multisig. Also tests that rewards are distributed as expected for voting and signing.
 #[test]
-fn basic_message_routing() {
+fn single_message_can_be_verified_and_routed_and_proven_and_rewards_are_distributed() {
     let (mut protocol, chain1, chain2, workers) = setup_test_case();
 
     let msgs = vec![Message {
