7b3b3954a825fcfa7e22b6c18a19fbd81a88b2c6
Edwin Guajardo <edwin@axelar.network>
2023-12-05 13:28:50 -0600

    update hash function to sort before hashing

--- packages/axelar-wasm-std/src/operators.rs
+++ packages/axelar-wasm-std/src/operators.rs
@@ -1,6 +1,7 @@
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{to_binary, HexBinary, Uint256};
 
+use itertools::Itertools;
 use sha3::{Digest, Keccak256};
 
 pub type OperatorsHash = [u8; 32];
@@ -15,7 +16,8 @@ impl Operators {
     pub fn hash(&self) -> OperatorsHash {
         let mut hasher = Keccak256::new();
         hasher.update(
-            to_binary(&self.weights_by_addresses).expect("could not serialize serializable object"),
+            to_binary(&self.weights_by_addresses.iter().sorted().collect_vec())
+                .expect("could not serialize serializable object"),
         );
         hasher.update(self.threshold.to_be_bytes());
         hasher.finalize().into()
@@ -32,13 +34,23 @@ mod tests {
     #[test]
     fn hash_id_unchanged() {
         let expected_operators_hash =
-            "a80417c50895c34c73dee98112e288a3b8dce0d9d77d42cbfbf6bbe29433770b";
+            "9ac8e5863ef6d306ad48dc0fdab30144921b2043b32e72c521dba71627bea747";
 
         let operators = Operators {
-            weights_by_addresses: vec![(
-                HexBinary::from_hex("6C51eec96bf0a8ec799cdD0Bbcb4512f8334Afe8").unwrap(),
-                Uint256::one(),
-            )],
+            weights_by_addresses: vec![
+                (
+                    HexBinary::from_hex("6C51eec96bf0a8ec799cdD0Bbcb4512f8334Afe8").unwrap(),
+                    Uint256::one(),
+                ),
+                (
+                    HexBinary::from_hex("11C67adfe52a3782bd518294188C4AAfaaF6cDeb").unwrap(),
+                    Uint256::one(),
+                ),
+                (
+                    HexBinary::from_hex("4905FD2e40B1A037256e32fe1e4BCa41AE510d73").unwrap(),
+                    Uint256::one(),
+                ),
+            ],
             threshold: Uint256::one(),
         };
 
