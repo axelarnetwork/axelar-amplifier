eac6f27f8e45aa09480c84dda2a406cf1aac1314
thhachain <thanh.ha@anchain.ai>
2024-01-10 12:00:24 -0800

    Adding flow access proto to tonic build compile

--- ampd/build.rs
+++ ampd/build.rs
@@ -1,6 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
-        .compile(&["proto/tofnd/multisig.proto"], &["proto/tofnd"])?;
+        .compile(
+            &["proto/tofnd/multisig.proto", "proto/flow/access.proto"],
+            &["proto/tofnd", "proto/flow"]
+        )?;
     Ok(())
 }
