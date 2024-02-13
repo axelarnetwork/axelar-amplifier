3b2941a82c6e970563520f21994be1251d317ffb
Edwin Guajardo <edwin@axelar.network>
2024-02-13 17:28:17 -0600

    add signature verifier api

---
+++ packages/signature-verifier-api/src/client.rs
@@ -0,0 +1,44 @@
+use cosmwasm_schema::serde::de::DeserializeOwned;
+use cosmwasm_std::{to_binary, Addr, HexBinary, QuerierWrapper, QueryRequest, Uint64, WasmQuery};
+use error_stack::{Result, ResultExt};
+
+use crate::msg::QueryMsg;
+
+pub struct SignatureVerifier<'a> {
+    pub address: Addr,
+    pub querier: QuerierWrapper<'a>,
+}
+
+impl SignatureVerifier<'_> {
+    fn query<U: DeserializeOwned + 'static>(&self, msg: &QueryMsg) -> Result<U, Error> {
+        self.querier
+            .query(&QueryRequest::Wasm(WasmQuery::Smart {
+                contract_addr: self.address.to_string(),
+                msg: to_binary(msg).expect("msg should always be serializable"),
+            }))
+            .change_context(Error::QuerySignatureVerifier)
+    }
+
+    pub fn verify_signature(
+        &self,
+        signature: HexBinary,
+        message: HexBinary,
+        public_key: HexBinary,
+        signer_address: String,
+        session_id: Uint64,
+    ) -> Result<bool, Error> {
+        self.query(&QueryMsg::VerifySignature {
+            signature,
+            message,
+            public_key,
+            signer_address,
+            session_id,
+        })
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("could not query the signature verifier contract")]
+    QuerySignatureVerifier,
+}
