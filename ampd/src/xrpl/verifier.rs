25d0e906d1835c19f5b7f293dfb878bb6699eedf
Alex Brezas <abresas@gmail.com>
2024-02-16 13:04:33 +0100

    ampd: xrpl memo payload handling with fee, relayer, amount, currencyg

--- ampd/src/xrpl/verifier.rs
+++ ampd/src/xrpl/verifier.rs
@@ -2,12 +2,15 @@ use std::collections::HashMap;
 use std::str::FromStr;
 
 use crate::types::Hash;
+use serde::Serialize;
 use connection_router::state::ChainName;
 use k256::sha2::Sha256;
+use serde_json::to_string;
 use sha3::Digest;
 use xrpl_http_client::ResultCategory;
 use xrpl_http_client::{Memo, Transaction::Payment, Transaction};
 use axelar_wasm_std::voting::Vote;
+use cosmwasm_std::{Uint256, HexBinary};
 
 use crate::handlers::xrpl_verify_msg::Message;
 use crate::xrpl::types::XRPLAddress;
@@ -52,6 +55,15 @@ pub fn is_successful_tx(tx: &Transaction) -> bool {
     return false;
 }
 
+#[derive(Serialize, Debug, PartialEq)]
+struct MemoPayload {
+    fee: Uint256,
+    relayer: String,
+    amount: Uint256,
+    currency: String,
+    payload_hash: HexBinary
+}
+
 pub fn verify_memos(memos: Vec<Memo>, message: &Message) -> bool {
     let memo_kv: HashMap<String, String> = memos
         .into_iter()
@@ -59,13 +71,23 @@ pub fn verify_memos(memos: Vec<Memo>, message: &Message) -> bool {
         .map(|m| (m.memo_type.unwrap(), m.memo_data.unwrap()))
         .collect();
 
-    // TODO: how are memo types converted to strings here, maybe they need hex decoding?
-    memo_kv.get("destination_address") == Some(&message.destination_address)
-    && memo_kv.get("destination_chain") == Some(&message.destination_chain.to_string())
-    && (memo_kv.get("payload_hash") == Some(&message.payload_hash.to_string()) || (memo_kv.contains_key("payload") && verify_payload(memo_kv.get("payload").unwrap(), &message.payload_hash)))
-}
-
-// TODO: is ethers Hash type ok here?
-pub fn verify_payload(payload: &String, payload_hash: &Hash) -> bool {
-    Sha256::digest(payload) == payload_hash.to_fixed_bytes().into()
+    || -> Option<bool> {
+        let memo_payload_hash = memo_kv
+            .get("payload_hash")
+            .cloned()
+            .or_else(|| {
+                memo_kv.get("payload").map(|p| hex::encode(Sha256::digest(p).to_vec()))
+            })?;
+        let memo_payload = MemoPayload {
+            fee: Uint256::from_str(memo_kv.get("fee")?).ok()?,
+            relayer: memo_kv.get("relayer")?.clone(),
+            amount: Uint256::from_str(memo_kv.get("amount")?).ok()?,
+            currency: memo_kv.get("currency")?.clone(),
+            payload_hash: HexBinary::from_hex(memo_payload_hash.as_str()).ok()?
+        };
+        let memo_hash: [u8; 32] = Sha256::digest(to_string(&memo_payload).ok()?).into();
+        Some(memo_kv.get("destination_address") == Some(&message.destination_address)
+        && memo_kv.get("destination_chain") == Some(&message.destination_chain.to_string())
+        && *message.payload_hash.as_fixed_bytes() == memo_hash)
+    }().unwrap_or(false)
 }
