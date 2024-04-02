c3703d54137351a689e290e6dcc79ced052b39a8
Houmaan Chamani <houmaan@interoplabs.io>
2024-04-02 16:29:48 -0400

    feat: register chain and prover contract with monitoring (#325)
    * feat: add CONTRACTS_PER_CHAIN map and execution message
    
    * test: add test_instantiation to monitoring
    
    * fix: remove redundant error types, address clippy lint issues
    
    * refactor: change scope to only include prover, add tests
    
    * fix: rollback changes made to rewards contract
    
    * fix: cargo formatting for rewards contract
    
    * refactor: address PR open conversations
    
    * refactor: add more description to Unauthorized and NoProversRegisteredForChain errors

--- contracts/monitoring/src/contract.rs
+++ contracts/monitoring/src/contract.rs
@@ -4,6 +4,8 @@ use crate::state::{Config, CONFIG};
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 
+use crate::execute;
+
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
@@ -21,14 +23,22 @@ pub fn instantiate(
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-#[allow(dead_code)]
 pub fn execute(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
-    _info: MessageInfo,
-    _msg: ExecuteMsg,
+    info: MessageInfo,
+    msg: ExecuteMsg,
 ) -> Result<Response, axelar_wasm_std::ContractError> {
-    Ok(Response::new())
+    match msg {
+        ExecuteMsg::RegisterProverContract {
+            chain_name,
+            new_prover_addr,
+        } => {
+            execute::check_governance(&deps, info)?;
+            execute::register_prover(deps, chain_name, new_prover_addr)
+        }
+    }
+    .map_err(axelar_wasm_std::ContractError::from)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -38,5 +48,99 @@ pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
         QueryMsg::GetActiveVerifiersForChain { chain: _ } => {
             todo!()
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::error::ContractError;
+    use crate::query;
+    use connection_router_api::ChainName;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+    use cosmwasm_std::Addr;
+
+    use super::*;
+
+    #[test]
+    #[allow(clippy::arithmetic_side_effects)]
+    fn test_instantiation() {
+        let governance = "governance_for_monitoring";
+        let mut deps = mock_dependencies();
+        let info = mock_info("instantiator", &[]);
+        let env = mock_env();
+
+        let res = instantiate(
+            deps.as_mut(),
+            env,
+            info,
+            InstantiateMsg {
+                governance_address: governance.to_string(),
+            },
+        );
+        assert!(res.is_ok());
+
+        let config = CONFIG.load(deps.as_ref().storage).unwrap();
+        assert_eq!(config.governance, governance);
+    }
+
+    #[test]
+    fn add_prover_from_goverance_succeeds() {
+        let governance = "governance_for_monitoring";
+        let mut deps = mock_dependencies();
+        let info = mock_info("instantiator", &[]);
+        let env = mock_env();
+
+        let _ = instantiate(
+            deps.as_mut(),
+            env.clone(),
+            info,
+            InstantiateMsg {
+                governance_address: governance.to_string(),
+            },
+        );
+
+        let eth_prover = Addr::unchecked("eth_prover");
+        let eth: ChainName = "Ethereum".to_string().try_into().unwrap();
+        let msg = ExecuteMsg::RegisterProverContract {
+            chain_name: eth.clone(),
+            new_prover_addr: eth_prover.clone(),
+        };
+        let _res = execute(deps.as_mut(), mock_env(), mock_info(governance, &[]), msg).unwrap();
+        let chain_provers = query::provers(deps.as_ref(), eth.clone()).unwrap();
+        assert_eq!(chain_provers, vec![eth_prover]);
+    }
+
+    #[test]
+    fn add_prover_from_random_address_fails() {
+        let governance = "governance_for_monitoring";
+        let mut deps = mock_dependencies();
+        let info = mock_info("instantiator", &[]);
+        let env = mock_env();
+
+        let _ = instantiate(
+            deps.as_mut(),
+            env.clone(),
+            info,
+            InstantiateMsg {
+                governance_address: governance.to_string(),
+            },
+        );
+
+        let eth_prover = Addr::unchecked("eth_prover");
+        let eth: ChainName = "Ethereum".to_string().try_into().unwrap();
+        let msg = ExecuteMsg::RegisterProverContract {
+            chain_name: eth.clone(),
+            new_prover_addr: eth_prover.clone(),
+        };
+        let res = execute(
+            deps.as_mut(),
+            env.clone(),
+            mock_info("random_address", &[]),
+            msg,
+        );
+        assert_eq!(
+            res.unwrap_err().to_string(),
+            axelar_wasm_std::ContractError::from(ContractError::Unauthorized).to_string()
+        );
     }
 }
