27abfcaa6ff8df99a41e820cd6fa4bce7155c764
maancham <houmaan@interoplabs.io>
2024-04-16 11:09:16 -0400

    refactor: add TestSetup struct to monitoring unit tests and cleanup

--- contracts/monitoring/src/contract.rs
+++ contracts/monitoring/src/contract.rs
@@ -63,13 +63,45 @@ mod tests {
     use crate::error::ContractError;
     use axelar_wasm_std::Participant;
     use connection_router_api::ChainName;
-    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::{Addr, HexBinary, Uint256};
+    use cosmwasm_std::testing::{
+        mock_dependencies, mock_env, mock_info, MockApi, MockQuerier, MockStorage,
+    };
+    use cosmwasm_std::{Addr, Empty, HexBinary, OwnedDeps, Uint256};
     use multisig::key::{KeyType, PublicKey};
     use multisig::worker_set::WorkerSet;
     use tofn::ecdsa::KeyPair;
 
     use super::*;
+
+    struct TestSetup {
+        deps: OwnedDeps<MockStorage, MockApi, MockQuerier, Empty>,
+        env: Env,
+        prover: Addr,
+        chain_name: ChainName,
+    }
+
+    fn setup(governance: &str) -> TestSetup {
+        let mut deps = mock_dependencies();
+        let info = mock_info("instantiator", &[]);
+        let env = mock_env();
+
+        let instantiate_msg = InstantiateMsg {
+            governance_address: governance.to_string(),
+        };
+
+        let res = instantiate(deps.as_mut(), env.clone(), info.clone(), instantiate_msg);
+        assert!(res.is_ok());
+
+        let eth_prover = Addr::unchecked("eth_prover");
+        let eth: ChainName = "Ethereum".to_string().try_into().unwrap();
+
+        TestSetup {
+            deps,
+            env,
+            prover: eth_prover,
+            chain_name: eth,
+        }
+    }
 
     pub struct Worker {
         pub addr: Addr,
@@ -122,78 +154,46 @@ mod tests {
     #[allow(clippy::arithmetic_side_effects)]
     fn test_instantiation() {
         let governance = "governance_for_monitoring";
-        let mut deps = mock_dependencies();
-        let info = mock_info("instantiator", &[]);
-        let env = mock_env();
+        let test_setup = setup(governance);
 
-        let res = instantiate(
-            deps.as_mut(),
-            env,
-            info,
-            InstantiateMsg {
-                governance_address: governance.to_string(),
-            },
-        );
-        assert!(res.is_ok());
-
-        let config = CONFIG.load(deps.as_ref().storage).unwrap();
+        let config = CONFIG.load(test_setup.deps.as_ref().storage).unwrap();
         assert_eq!(config.governance, governance);
     }
 
     #[test]
     fn add_prover_from_governance_succeeds() {
         let governance = "governance_for_monitoring";
-        let mut deps = mock_dependencies();
-        let info = mock_info("instantiator", &[]);
-        let env = mock_env();
+        let mut test_setup = setup(governance);
 
-        let _ = instantiate(
-            deps.as_mut(),
-            env.clone(),
-            info,
-            InstantiateMsg {
-                governance_address: governance.to_string(),
+        let _res = execute(
+            test_setup.deps.as_mut(),
+            test_setup.env,
+            mock_info(governance, &[]),
+            ExecuteMsg::RegisterProverContract {
+                chain_name: test_setup.chain_name.clone(),
+                new_prover_addr: test_setup.prover.clone(),
             },
-        );
+        )
+        .unwrap();
 
-        let eth_prover = Addr::unchecked("eth_prover");
-        let eth: ChainName = "Ethereum".to_string().try_into().unwrap();
-        let msg = ExecuteMsg::RegisterProverContract {
-            chain_name: eth.clone(),
-            new_prover_addr: eth_prover.clone(),
-        };
-        let _res = execute(deps.as_mut(), mock_env(), mock_info(governance, &[]), msg).unwrap();
-        let chain_provers = query::provers(deps.as_ref(), eth.clone()).unwrap();
-        assert_eq!(chain_provers, vec![eth_prover]);
+        let chain_provers =
+            query::provers(test_setup.deps.as_ref(), test_setup.chain_name.clone()).unwrap();
+        assert_eq!(chain_provers, vec![test_setup.prover]);
     }
 
     #[test]
     fn add_prover_from_random_address_fails() {
         let governance = "governance_for_monitoring";
-        let mut deps = mock_dependencies();
-        let info = mock_info("instantiator", &[]);
-        let env = mock_env();
+        let mut test_setup = setup(governance);
 
-        let _ = instantiate(
-            deps.as_mut(),
-            env.clone(),
-            info,
-            InstantiateMsg {
-                governance_address: governance.to_string(),
-            },
-        );
-
-        let eth_prover = Addr::unchecked("eth_prover");
-        let eth: ChainName = "Ethereum".to_string().try_into().unwrap();
-        let msg = ExecuteMsg::RegisterProverContract {
-            chain_name: eth.clone(),
-            new_prover_addr: eth_prover.clone(),
-        };
         let res = execute(
-            deps.as_mut(),
-            env.clone(),
+            test_setup.deps.as_mut(),
+            test_setup.env,
             mock_info("random_address", &[]),
-            msg,
+            ExecuteMsg::RegisterProverContract {
+                chain_name: test_setup.chain_name.clone(),
+                new_prover_addr: test_setup.prover.clone(),
+            },
         );
         assert_eq!(
             res.unwrap_err().to_string(),
@@ -204,36 +204,24 @@ mod tests {
     #[test]
     fn set_and_get_populated_active_worker_set_success() {
         let governance = "governance_for_monitoring";
-        let mut deps = mock_dependencies();
-        let info = mock_info("instantiator", &[]);
-        let env = mock_env();
+        let mut test_setup = setup(governance);
 
-        let _ = instantiate(
-            deps.as_mut(),
-            env.clone(),
-            info,
-            InstantiateMsg {
-                governance_address: governance.to_string(),
-            },
-        );
-
-        let eth_prover = Addr::unchecked("eth_prover");
-        let eth: ChainName = "Ethereum".to_string().try_into().unwrap();
-
-        let new_worker = create_worker(1, Addr::unchecked("worker1"), vec![eth]);
-        let new_worker_set = create_worker_set_from_workers(&vec![new_worker], env.block.height);
+        let new_worker = create_worker(1, Addr::unchecked("worker1"), vec![test_setup.chain_name]);
+        let new_worker_set =
+            create_worker_set_from_workers(&vec![new_worker], test_setup.env.block.height);
 
         let _res = execute(
-            deps.as_mut(),
-            env.clone(),
-            mock_info(eth_prover.as_ref(), &[]),
+            test_setup.deps.as_mut(),
+            test_setup.env.clone(),
+            mock_info(test_setup.prover.as_ref(), &[]),
             ExecuteMsg::RegisterActiveWorkerSet {
                 next_worker_set: new_worker_set.clone(),
             },
         );
 
         let eth_active_worker_set =
-            query::get_active_worker_set(deps.as_ref(), eth_prover.clone()).unwrap();
+            query::get_active_worker_set(test_setup.deps.as_ref(), test_setup.prover.clone())
+                .unwrap();
 
         assert_eq!(eth_active_worker_set, new_worker_set);
     }
@@ -241,29 +229,20 @@ mod tests {
     #[test]
     fn set_and_get_empty_active_worker_set_success() {
         let governance = "governance_for_monitoring";
-        let mut deps = mock_dependencies();
-        let info = mock_info("instantiator", &[]);
-        let env = mock_env();
+        let mut test_setup = setup(governance);
 
-        let _ = instantiate(
-            deps.as_mut(),
-            env.clone(),
-            info,
-            InstantiateMsg {
-                governance_address: governance.to_string(),
+        let _response = execute(
+            test_setup.deps.as_mut(),
+            test_setup.env,
+            mock_info(governance, &[]),
+            ExecuteMsg::RegisterProverContract {
+                chain_name: test_setup.chain_name.clone(),
+                new_prover_addr: test_setup.prover.clone(),
             },
         );
 
-        let eth_prover = Addr::unchecked("eth_prover");
-        let eth: ChainName = "Ethereum".to_string().try_into().unwrap();
-        let msg = ExecuteMsg::RegisterProverContract {
-            chain_name: eth.clone(),
-            new_prover_addr: eth_prover.clone(),
-        };
-        let _response =
-            execute(deps.as_mut(), mock_env(), mock_info(governance, &[]), msg).unwrap();
-
-        let query_result = query::get_active_worker_set(deps.as_ref(), eth_prover.clone());
+        let query_result =
+            query::get_active_worker_set(test_setup.deps.as_ref(), test_setup.prover.clone());
 
         assert_eq!(
             query_result.unwrap_err().to_string(),
