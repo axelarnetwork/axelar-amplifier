4f90cf85d924f2ed832f52f26f824f9902c9d75f
CJ Cobb <cj@axelar.network>
2024-04-18 10:58:28 -0400

    review

--- contracts/multisig-prover/src/msg.rs
+++ contracts/multisig-prover/src/msg.rs
@@ -8,38 +8,46 @@ use crate::encoding::{Data, Encoder};
 
 #[cw_serde]
 pub struct InstantiateMsg {
-    // Address that can call UpdateWorkerSet
+    /// Address that can execute all messages that either have unrestricted or admin permission level, such as UpdateWorkerSet.
+    /// Should be set to a trusted address that can react to unexpected interruptions to the contract's operation.
     pub admin_address: String,
-    // Address that can call UpdateSigning threshold, as well as automatically confirm worker set updates, bypassing verification
+    /// Address that can call all messages of unrestricted, admin and governance permission level, such as UpdateSigningThreshold.
+    /// This address can execute messages that bypasses verification checks to rescue the contract if it got into an otherwise unrecoverable state due to external forces.
+    /// On mainnet, it should match the address of the Cosmos governance module.
     pub governance_address: String,
-    // Address of the gateway on axelar associated with the destination chain. For example, if this prover is creating proofs to
-    // be relayed to Ethereum, this is the address of the gateway on Axelar for Ethereum.
+    /// Address of the gateway on axelar associated with the destination chain. For example, if this prover is creating proofs to
+    /// be relayed to Ethereum, this is the address of the gateway on Axelar for Ethereum.
     pub gateway_address: String,
-    // Address of the multisig contract on axelar.
+    /// Address of the multisig contract on axelar.
     pub multisig_address: String,
-    // Address of the monitoring contract on axelar.
+    /// Address of the monitoring contract on axelar.
     pub monitoring_address: String,
-    // Address of the service registry contract on axelar.
+    /// Address of the service registry contract on axelar.
     pub service_registry_address: String,
-    // Address of the voting verifier contract on axelar associated with the destination chain. For example, if this prover is creating
-    // proofs to be relayed to Ethereum, this is the address of the voting verifier for Ethereum.
+    /// Address of the voting verifier contract on axelar associated with the destination chain. For example, if this prover is creating
+    /// proofs to be relayed to Ethereum, this is the address of the voting verifier for Ethereum.
     pub voting_verifier_address: String,
-    // Chain id of the chain for which this prover contract creates proofs. For example, if the destination chain is Ethereum, the chain id is 1.
+    /// Chain id of the chain for which this prover contract creates proofs. For example, if the destination chain is Ethereum, the chain id is 1.
     pub destination_chain_id: Uint256,
-    // Threshold of weighted signatures required for signing to be considered complete
+    /// Threshold of weighted signatures required for signing to be considered complete
     pub signing_threshold: MajorityThreshold,
-    // Name of service in the service registry for which verifiers are registered.
+    /// Name of service in the service registry for which verifiers are registered.
     pub service_name: String,
-    // Name of chain for which this prover contract creates proofs.
+    /// Name of chain for which this prover contract creates proofs.
     pub chain_name: String,
-    // Maximum tolerable difference between currently active workerset and registered workerset.
-    // The workerset registered in the service registry must be different by more than this number
-    // of workers before calling UpdateWorkerSet. For example, if this is set to 1, UpdateWorkerSet
-    // will fail unless the registered workerset and active workerset differ by more than 1.
+    /// Maximum tolerable difference between currently active workerset and registered workerset.
+    /// The workerset registered in the service registry must be different by more than this number
+    /// of workers before calling UpdateWorkerSet. For example, if this is set to 1, UpdateWorkerSet
+    /// will fail unless the registered workerset and active workerset differ by more than 1.
     pub worker_set_diff_threshold: u32,
-    // Type of encoding to use for signed batches.
+    /// Type of encoding to use for signed batches. Blockchains can encode their execution payloads in various ways (ABI, BCS, etc).
+    /// This defines the specific encoding type to use for this prover, which should correspond to the encoding type used by the gateway
+    /// deployed on the destination chain.
     pub encoder: Encoder,
-    // Key type to use for signing.
+    /// Public key type verifiers use for signing batches. Different blockchains support different cryptographic signature algorithms (ECDSA, Ed25519, etc).
+    /// This defines the specific signature algorithm to use for this prover, which should correspond to the signature algorithm used by the gateway
+    /// deployed on the destination chain. The multisig contract supports multiple public keys per verifier (each a different type of key), and this
+    /// parameter controls which registered public key to use for signing for each verifier registered to the destination chain.
     pub key_type: KeyType,
 }
 
