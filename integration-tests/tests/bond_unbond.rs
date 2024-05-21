13d6ba119ef9dcce54fce641ffc6397346e5c404
maancham <houmaan@interoplabs.io>
2024-05-20 21:03:00 -0400

    test: two more failure integration test paths for can_unbond

--- integration-tests/tests/bond_unbond.rs
+++ integration-tests/tests/bond_unbond.rs
@@ -158,3 +158,108 @@ fn claim_stake_when_in_some_active_worker_sets_fails() {
         assert!(claim_result.is_err());
     }
 }
+
+#[test]
+fn claim_stake_after_deregistering_before_rotation_fails() {
+    let chains: Vec<router_api::ChainName> = vec![
+        "Ethereum".to_string().try_into().unwrap(),
+        "Polygon".to_string().try_into().unwrap(),
+    ];
+
+    let test_utils::TestCase {
+        mut protocol,
+        workers,
+        min_worker_bond,
+        ..
+    } = test_utils::setup_test_case();
+
+    let new_workers = test_utils::create_new_workers_vec(
+        chains.clone(),
+        vec![("worker3".to_string(), 3), ("worker4".to_string(), 4)],
+    );
+
+    test_utils::register_workers(&mut protocol, &new_workers, min_worker_bond);
+
+    for worker in &workers {
+        let response = protocol.service_registry.execute(
+            &mut protocol.app,
+            worker.addr.clone(),
+            &ExecuteMsg::DeregisterChainSupport {
+                service_name: protocol.service_name.to_string(),
+                chains: chains.clone(),
+            },
+        );
+        assert!(response.is_ok());
+    }
+
+    for worker in &workers {
+        let response = protocol.service_registry.execute(
+            &mut protocol.app,
+            worker.addr.clone(),
+            &ExecuteMsg::UnbondWorker {
+                service_name: protocol.service_name.to_string(),
+            },
+        );
+        assert!(response.is_ok());
+    }
+
+    let claim_results = test_utils::claim_stakes(&mut protocol, &workers);
+    for claim_result in claim_results {
+        assert!(claim_result.is_err());
+    }
+}
+
+#[test]
+fn claim_stake_when_jailed_fails() {
+    let chains: Vec<router_api::ChainName> = vec![
+        "Ethereum".to_string().try_into().unwrap(),
+        "Polygon".to_string().try_into().unwrap(),
+    ];
+
+    let test_utils::TestCase {
+        mut protocol,
+        chain1: ethereum,
+        chain2: polygon,
+        workers,
+        min_worker_bond,
+        ..
+    } = test_utils::setup_test_case();
+
+    let new_workers = test_utils::create_new_workers_vec(
+        chains.clone(),
+        vec![("worker3".to_string(), 3), ("worker4".to_string(), 4)],
+    );
+
+    test_utils::register_workers(&mut protocol, &new_workers, min_worker_bond);
+
+    test_utils::deregister_workers(&mut protocol, &workers);
+
+    test_utils::rotate_active_worker_set(&mut protocol, ethereum, &workers, &new_workers);
+    test_utils::rotate_active_worker_set(&mut protocol, polygon, &workers, &new_workers);
+
+    for worker in &workers {
+        let response = protocol.service_registry.execute(
+            &mut protocol.app,
+            worker.addr.clone(),
+            &ExecuteMsg::UnbondWorker {
+                service_name: protocol.service_name.to_string(),
+            },
+        );
+        assert!(response.is_ok());
+    }
+
+    let response = protocol.service_registry.execute(
+        &mut protocol.app,
+        protocol.governance_address.clone(),
+        &ExecuteMsg::JailWorkers {
+            service_name: protocol.service_name.to_string(),
+            workers: vec!["worker1".to_string(), "worker2".to_string()],
+        },
+    );
+    assert!(response.is_ok());
+
+    let claim_results = test_utils::claim_stakes(&mut protocol, &workers);
+    for claim_result in claim_results {
+        assert!(claim_result.clone().is_err());
+    }
+}
