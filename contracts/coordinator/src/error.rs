b61a78e3ee387a3f41738a50d214acbbbc3b5773
Edwin Guajardo <edwin@axelar.network>
2024-05-24 11:17:05 -0600

    rename worker to verifier

--- contracts/coordinator/src/error.rs
+++ contracts/coordinator/src/error.rs
@@ -14,6 +14,6 @@ pub enum ContractError {
     #[error("no provers registered for chain {0}")]
     NoProversRegisteredForChain(ChainName),
 
-    #[error("no active worker set registered")]
-    NoActiveWorkerSetRegistered,
+    #[error("no active verifier set registered")]
+    NoActiveVerifierSetRegistered,
 }
