74e2f75c1b677c0b5695b5db182b6c3b7550efd8
CJ Cobb <46455409+cjcobb23@users.noreply.github.com>
2024-05-24 16:05:51 -0400

    feat(minor-multisig)!: migrate worker set to verifier set (#420)
    * feat(minor-multisig)!: migrate worker set to verifier set

--- contracts/multisig/src/contract.rs
+++ contracts/multisig/src/contract.rs
@@ -7,6 +7,7 @@ use cosmwasm_std::{
 
 use crate::{
     events::Event,
+    migrations,
     msg::{ExecuteMsg, InstantiateMsg, QueryMsg},
     state::{
         get_verifier_set, Config, CONFIG, SIGNING_SESSIONS, SIGNING_SESSION_COUNTER, VERIFIER_SETS,
@@ -23,12 +24,13 @@ const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
     _msg: Empty,
 ) -> Result<Response, axelar_wasm_std::ContractError> {
-    // TODO migrate
-    Ok(Response::default())
+    cw2::set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    migrations::v_0_3::migrate_verifier_sets(deps)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
