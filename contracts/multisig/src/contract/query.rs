62bb4ef7c03c76160af4336ab01a828bc5f67d8b
eguajardo <edwin@axelar.network>
2024-05-24 18:07:18 -0600

    rename worker to verifier (#427)
    Co-authored-by: Milap Sheth <milap@interoplabs.io>

--- contracts/multisig/src/contract/query.rs
+++ contracts/multisig/src/contract/query.rs
@@ -20,11 +20,11 @@ pub fn get_multisig(deps: Deps, session_id: Uint64) -> StdResult<Multisig> {
     })
 }
 
-pub fn get_verifier_set(deps: Deps, worker_set_id: String) -> StdResult<VerifierSet> {
-    VERIFIER_SETS.load(deps.storage, &worker_set_id)
+pub fn get_verifier_set(deps: Deps, verifier_set_id: String) -> StdResult<VerifierSet> {
+    VERIFIER_SETS.load(deps.storage, &verifier_set_id)
 }
 
-pub fn get_public_key(deps: Deps, worker: Addr, key_type: KeyType) -> StdResult<PublicKey> {
-    let raw = load_pub_key(deps.storage, worker, key_type)?;
+pub fn get_public_key(deps: Deps, verifier: Addr, key_type: KeyType) -> StdResult<PublicKey> {
+    let raw = load_pub_key(deps.storage, verifier, key_type)?;
     Ok(PublicKey::try_from((key_type, raw)).expect("could not decode pub key"))
 }
