148dd064b80e92690475598e13cad40963eaccae
Christian Gorenflo <christian@axelar.network>
2024-05-27 14:56:22 -0400

    refactor(voting-verifier): rename worker to verifier

--- contracts/voting-verifier/src/contract.rs
+++ contracts/voting-verifier/src/contract.rs
@@ -542,7 +542,7 @@ mod test {
         );
         assert!(res.is_ok());
 
-        // 2. Workers cast votes, but only reach consensus on the first three messages
+        // 2. Verifiers cast votes, but only reach consensus on the first three messages
 
         verifiers.iter().enumerate().for_each(|(i, verifier)| {
             let msg = ExecuteMsg::Vote {
@@ -833,7 +833,7 @@ mod test {
     }
 
     #[test]
-    fn should_start_worker_set_confirmation() {
+    fn should_start_verifier_set_confirmation() {
         let msg_id_format = MessageIdFormat::HexTxHashAndEventIndex;
         let verifiers = verifiers(2);
         let mut deps = setup(verifiers.clone(), &msg_id_format);
@@ -862,7 +862,7 @@ mod test {
     }
 
     #[test]
-    fn should_confirm_worker_set() {
+    fn should_confirm_verifier_set() {
         let msg_id_format = MessageIdFormat::HexTxHashAndEventIndex;
         let verifiers = verifiers(2);
         let mut deps = setup(verifiers.clone(), &msg_id_format);
@@ -915,7 +915,7 @@ mod test {
     }
 
     #[test]
-    fn should_not_confirm_worker_set() {
+    fn should_not_confirm_verifier_set() {
         let msg_id_format = MessageIdFormat::HexTxHashAndEventIndex;
         let verifiers = verifiers(2);
         let mut deps = setup(verifiers.clone(), &msg_id_format);
@@ -971,7 +971,7 @@ mod test {
     }
 
     #[test]
-    fn should_confirm_worker_set_after_failed() {
+    fn should_confirm_verifier_set_after_failed() {
         let msg_id_format = MessageIdFormat::HexTxHashAndEventIndex;
         let verifiers = verifiers(2);
         let mut deps = setup(verifiers.clone(), &msg_id_format);
