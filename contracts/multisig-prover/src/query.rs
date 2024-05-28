ca62aa06c96105d672c63f5dd048968ae56f549c
jcs47 <11947034+jcs47@users.noreply.github.com>
2024-05-28 22:50:52 +0100

    feat(multisig-prover): add `get_next_verifier_set` query

--- contracts/multisig-prover/src/query.rs
+++ contracts/multisig-prover/src/query.rs
@@ -52,3 +52,7 @@ pub fn get_proof(
 pub fn get_verifier_set(deps: Deps) -> StdResult<Option<VerifierSet>> {
     CURRENT_VERIFIER_SET.may_load(deps.storage)
 }
+
+pub fn get_next_verifier_set(deps: Deps) -> StdResult<Option<VerifierSet>> {
+    NEXT_VERIFIER_SET.may_load(deps.storage)
+}
