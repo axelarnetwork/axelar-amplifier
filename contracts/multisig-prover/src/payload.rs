9cea7de3cca985d4819decfbc32f806baac45e0f
Haiyi Zhong <haiyizxx@gmail.com>
2024-06-03 16:57:54 -0400

    refactor(multisig-prover): use evm-gateway package

--- contracts/multisig-prover/src/payload.rs
+++ contracts/multisig-prover/src/payload.rs
@@ -1,6 +1,7 @@
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{from_json, HexBinary, StdResult};
 use cw_storage_plus::{Key, KeyDeserialize, PrimaryKey};
+use error_stack::Result;
 use sha3::{Digest, Keccak256};
 
 use axelar_wasm_std::hash::Hash;
@@ -55,6 +56,7 @@ impl Payload {
             Payload::VerifierSet(_) => None,
         }
     }
+
     pub fn execute_data(
         &self,
         encoder: Encoder,
