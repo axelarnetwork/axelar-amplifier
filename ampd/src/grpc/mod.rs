04b1b5db8fbea2d42d6bfad74e8c97770970b7c1
Sammy <sammy.liu@axelar.network>
2024-06-10 11:50:07 -0400

    feat(ampd): create the ampd gRPC client (#447)
    * feat(ampd): create the ampd gRPC client
    
    * fix imports

--- ampd/src/grpc/mod.rs
+++ ampd/src/grpc/mod.rs
@@ -1,27 +1,6 @@
-use tonic::transport::{server::Router, Server};
-
-use crate::event_sub::EventSubscriber;
-use crate::queue::queued_broadcaster::QueuedBroadcasterClient;
-use crate::tofnd::grpc::MultisigClient;
-
-mod proto {
+pub mod proto {
     tonic::include_proto!("ampd");
 }
-mod ampd;
-mod crypto;
 
-#[allow(dead_code)]
-pub fn new_server(
-    event_subscriber: EventSubscriber,
-    broadcaster: QueuedBroadcasterClient,
-    multisig_client: MultisigClient,
-) -> Router {
-    Server::builder()
-        .add_service(proto::ampd_server::AmpdServer::new(ampd::Server::new(
-            event_subscriber,
-            broadcaster,
-        )))
-        .add_service(proto::crypto_server::CryptoServer::new(
-            crypto::Server::new(multisig_client),
-        ))
-}
+pub mod client;
+pub mod server;
