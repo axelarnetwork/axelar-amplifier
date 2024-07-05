0f40644f63d9cbe01ba12d25a2ae9b091c328412
Christian Gorenflo <christian@axelar.network>
2024-07-05 09:33:06 -0400

    refactor(coordinator): use generalized permission control (#485)

--- contracts/coordinator/src/lib.rs
+++ contracts/coordinator/src/lib.rs
@@ -1,6 +1,4 @@
 pub mod contract;
 pub mod error;
-pub mod execute;
 pub mod msg;
-pub mod query;
-pub mod state;
+mod state;
--- contracts/router/src/lib.rs
+++ contracts/router/src/lib.rs
@@ -1,5 +1,4 @@
 pub mod contract;
 pub mod events;
-mod migrations;
 pub mod msg;
 pub mod state;
