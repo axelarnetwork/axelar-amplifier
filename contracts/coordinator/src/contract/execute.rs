ca9d758cf984c5ec05f399b872e39e99d630e437
Edwin Guajardo <edwin@axelar.network>
2024-07-10 08:41:47 -0600

    update permission control to set verifiers

--- contracts/coordinator/src/contract/execute.rs
+++ contracts/coordinator/src/contract/execute.rs
@@ -4,14 +4,14 @@ use std::collections::HashSet;
 use router_api::ChainName;
 
 use crate::error::ContractError;
-use crate::state::{update_verifier_set_for_prover, PROVER_PER_CHAIN};
+use crate::state::{save_prover_for_chain, update_verifier_set_for_prover};
 
 pub fn register_prover(
     deps: DepsMut,
     chain_name: ChainName,
     new_prover_addr: Addr,
 ) -> Result<Response, ContractError> {
-    PROVER_PER_CHAIN.save(deps.storage, chain_name.clone(), &(new_prover_addr))?;
+    save_prover_for_chain(deps.storage, chain_name, new_prover_addr)?;
     Ok(Response::new())
 }
 
--- contracts/coordinator/src/state.rs
+++ contracts/coordinator/src/state.rs
@@ -1,18 +1,86 @@
 use crate::error::ContractError;
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Order, Storage};
-use cw_storage_plus::{Index, IndexList, IndexedMap, Map, MultiIndex};
+use cw_storage_plus::{Index, IndexList, IndexedMap, MultiIndex, UniqueIndex};
 use router_api::ChainName;
 use std::collections::HashSet;
 
 pub const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
 pub const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 type ProverAddress = Addr;
+type VerifierAddress = Addr;
 
-pub const PROVER_PER_CHAIN: Map<ChainName, ProverAddress> = Map::new("prover_per_chain");
+struct ChainProverIndexes<'a> {
+    pub by_chain: UniqueIndex<'a, ChainName, ChainProverRecord, (ChainName, ProverAddress)>,
+    pub by_prover: UniqueIndex<'a, ProverAddress, ChainProverRecord, (ChainName, ProverAddress)>,
+}
+
+impl<'a> IndexList<ChainProverRecord> for ChainProverIndexes<'a> {
+    fn get_indexes(&self) -> Box<dyn Iterator<Item = &dyn Index<ChainProverRecord>> + '_> {
+        let v: Vec<&dyn Index<ChainProverRecord>> = vec![&self.by_chain, &self.by_prover];
+        Box::new(v.into_iter())
+    }
+}
+
+#[cw_serde]
+pub struct ChainProverRecord {
+    pub prover: ProverAddress,
+    pub chain: ChainName,
+}
+
+const CHAIN_PROVER_INDEXED_MAP: IndexedMap<
+    (ChainName, ProverAddress),
+    ChainProverRecord,
+    ChainProverIndexes,
+> = IndexedMap::new(
+    "chain_prover_map",
+    ChainProverIndexes {
+        by_chain: UniqueIndex::new(|d| d.chain.clone(), "chain_prover_map_by_chain"),
+        by_prover: UniqueIndex::new(|d| d.prover.clone(), "chain_prover_map_by_prover"),
+    },
+);
+
+pub fn load_chain_by_prover(
+    storage: &dyn Storage,
+    prover_address: ProverAddress,
+) -> Result<ChainProverRecord, ContractError> {
+    CHAIN_PROVER_INDEXED_MAP
+        .idx
+        .by_prover
+        .item(storage, prover_address)?
+        .map(|(_, r)| r)
+        .ok_or(ContractError::ProverNotRegistered)
+}
+
+#[allow(dead_code)] // Used in tests, might be useful in future query
+pub fn load_prover_by_chain(
+    storage: &dyn Storage,
+    chain_name: ChainName,
+) -> Result<ChainProverRecord, ContractError> {
+    CHAIN_PROVER_INDEXED_MAP
+        .idx
+        .by_chain
+        .item(storage, chain_name)?
+        .map(|(_, r)| r)
+        .ok_or(ContractError::ProverNotRegistered)
+}
+
+pub fn save_prover_for_chain(
+    storage: &mut dyn Storage,
+    chain: ChainName,
+    prover: ProverAddress,
+) -> Result<(), ContractError> {
+    CHAIN_PROVER_INDEXED_MAP.save(
+        storage,
+        (chain.clone(), prover.clone()),
+        &ChainProverRecord { prover, chain },
+    )?;
+    Ok(())
+}
 
 pub struct VerifierSetIndex<'a> {
-    pub by_verifier: MultiIndex<'a, Addr, VerifierProverRecord, (Addr, Addr)>,
+    pub by_verifier:
+        MultiIndex<'a, VerifierAddress, VerifierProverRecord, (ProverAddress, VerifierAddress)>,
 }
 
 impl<'a> IndexList<VerifierProverRecord> for VerifierSetIndex<'a> {
@@ -25,11 +93,11 @@ impl<'a> IndexList<VerifierProverRecord> for VerifierSetIndex<'a> {
 #[cw_serde]
 pub struct VerifierProverRecord {
     pub prover: ProverAddress,
-    pub verifier: Addr,
+    pub verifier: VerifierAddress,
 }
 
 pub const VERIFIER_PROVER_INDEXED_MAP: IndexedMap<
-    (Addr, Addr),
+    (ProverAddress, VerifierAddress),
     VerifierProverRecord,
     VerifierSetIndex,
 > = IndexedMap::new(
@@ -46,13 +114,13 @@ pub const VERIFIER_PROVER_INDEXED_MAP: IndexedMap<
 pub fn update_verifier_set_for_prover(
     storage: &mut dyn Storage,
     prover_address: ProverAddress,
-    new_verifiers: HashSet<Addr>,
+    new_verifiers: HashSet<VerifierAddress>,
 ) -> Result<(), ContractError> {
     let existing_verifiers = VERIFIER_PROVER_INDEXED_MAP
         .prefix(prover_address.clone())
         .keys(storage, None, None, Order::Ascending)
         .filter_map(Result::ok)
-        .collect::<HashSet<Addr>>();
+        .collect::<HashSet<VerifierAddress>>();
 
     for verifier in existing_verifiers.difference(&new_verifiers) {
         VERIFIER_PROVER_INDEXED_MAP
