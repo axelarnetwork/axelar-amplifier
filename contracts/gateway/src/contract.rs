c14c140bfde46928ac83767100601b4bdc21f2b7
Christian Gorenflo <christian@axelar.network>
2024-07-19 01:58:01 -0400

    fmt test

--- contracts/gateway/src/contract.rs
+++ contracts/gateway/src/contract.rs
@@ -4,6 +4,7 @@ use std::fmt::Debug;
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response};
 use gateway_api::msg::{ExecuteMsg, QueryMsg};
+
 use router_api::CrossChainId;
 
 use crate::msg::InstantiateMsg;
