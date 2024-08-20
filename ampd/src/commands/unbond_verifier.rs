7cf0727b9bb767dc6fb2628a86021f3eaff9d864
CJ Cobb <46455409+cjcobb23@users.noreply.github.com>
2024-08-20 12:58:33 -0600

    feat(minor-ampd): add commands to unbond and claim stake (#587)

---
+++ ampd/src/commands/unbond_verifier.rs
@@ -0,0 +1,41 @@
+use axelar_wasm_std::nonempty;
+use cosmrs::cosmwasm::MsgExecuteContract;
+use cosmrs::tx::Msg;
+use error_stack::Result;
+use report::ResultCompatExt;
+use service_registry::msg::ExecuteMsg;
+use valuable::Valuable;
+
+use crate::commands::{broadcast_tx, verifier_pub_key};
+use crate::config::Config;
+use crate::{Error, PREFIX};
+
+#[derive(clap::Args, Debug, Valuable)]
+pub struct Args {
+    pub service_name: nonempty::String,
+}
+
+pub async fn run(config: Config, args: Args) -> Result<Option<String>, Error> {
+    let pub_key = verifier_pub_key(config.tofnd_config.clone()).await?;
+
+    let msg = serde_json::to_vec(&ExecuteMsg::UnbondVerifier {
+        service_name: args.service_name.into(),
+    })
+    .expect("unbond verifier msg should be serializable");
+
+    let tx = MsgExecuteContract {
+        sender: pub_key.account_id(PREFIX).change_context(Error::Tofnd)?,
+        contract: config.service_registry.cosmwasm_contract.as_ref().clone(),
+        msg,
+        funds: vec![],
+    }
+    .into_any()
+    .expect("failed to serialize proto message");
+
+    let tx_hash = broadcast_tx(config, tx, pub_key).await?.txhash;
+
+    Ok(Some(format!(
+        "successfully broadcast unbond verifier transaction, tx hash: {}",
+        tx_hash
+    )))
+}
