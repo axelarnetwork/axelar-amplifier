0a37b8e7f1a060e9a3b3572d6613029094836bb9
Christian Gorenflo <christian@axelar.network>
2024-08-21 15:45:14 -0400

    feat(ampd): add uri to connection errors (#590)

--- ampd/src/commands/register_public_key.rs
+++ ampd/src/commands/register_public_key.rs
@@ -53,9 +53,10 @@ pub async fn run(config: Config, args: Args) -> Result<Option<String>, Error> {
 
     let tofnd_config = config.tofnd_config.clone();
 
-    let multisig_client = MultisigClient::new(tofnd_config.party_uid, tofnd_config.url)
+    let multisig_client = MultisigClient::new(tofnd_config.party_uid, tofnd_config.url.clone())
         .await
-        .change_context(Error::Connection)?;
+        .change_context(Error::Connection)
+        .attach_printable(tofnd_config.url)?;
     let multisig_key = multisig_client
         .keygen(&multisig_address.to_string(), args.key_type.into())
         .await
