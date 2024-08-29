b9650220c1f53103fd46ddfa9c07af056589a333
Christian Gorenflo <christian@axelar.network>
2024-08-28 23:39:29 -0400

    lint

--- contracts/axelarnet-gateway/src/lib.rs
+++ contracts/axelarnet-gateway/src/lib.rs
@@ -4,5 +4,6 @@ pub mod msg;
 mod state;
 
 mod clients;
-pub use clients::{external::AxelarExecutableMsg, gateway::Client};
+pub use clients::external::AxelarExecutableMsg;
+pub use clients::gateway::Client;
 pub use state::ExecutableMessage;
