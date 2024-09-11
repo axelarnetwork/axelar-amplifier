0da965d877bcf0a87fb9ffb69cfc499f0d6460be
Milap Sheth <milap@interoplabs.io>
2024-09-11 04:50:26 -0400

    feat(interchain-token-service): add its hub contract (#572)
    Co-authored-by: Sammy <sammy.liu@axelar.network>
    Co-authored-by: Christian Gorenflo <christian@axelar.network>
    Co-authored-by: Rareș <6453351+raress96@users.noreply.github.com>
    Co-authored-by: CJ Cobb <46455409+cjcobb23@users.noreply.github.com>
    Co-authored-by: jcs47 <11947034+jcs47@users.noreply.github.com>
    Co-authored-by: João Sousa <joaosousa@Joaos-MBP.home>
    Co-authored-by: haiyizxx <haiyizxx@gmail.com>

--- interchain-token-service/src/primitives.rs
+++ interchain-token-service/src/primitives.rs
@@ -1,8 +1,11 @@
+use std::fmt::Display;
+
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{HexBinary, Uint256};
-use router_api::ChainName;
+use router_api::ChainNameRaw;
 use strum::FromRepr;
 
+/// A unique 32-byte identifier for linked cross-chain tokens across ITS contracts.
 #[cw_serde]
 #[derive(Eq)]
 pub struct TokenId(
@@ -11,6 +14,13 @@ pub struct TokenId(
     [u8; 32],
 );
 
+impl Display for TokenId {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", hex::encode(self.0))
+    }
+}
+
+/// The supported types of token managers that can be deployed by ITS contracts.
 #[cw_serde]
 #[derive(Eq, Copy, FromRepr)]
 #[repr(u8)]
@@ -23,48 +33,75 @@ pub enum TokenManagerType {
     Gateway,
 }
 
-/// ITS message type that can be sent between ITS contracts for transfers/token deployments
-/// `ItsMessage` that are routed via the ITS hub get wrapped inside `ItsHubMessage`
+/// A message sent between ITS contracts to facilitate interchain transfers, token deployments, or token manager deployments.
+/// `Message` routed via the ITS hub get wrapped inside a [`HubMessage`]
 #[cw_serde]
-#[derive(Eq)]
-pub enum ItsMessage {
+#[derive(Eq, strum::IntoStaticStr)]
+pub enum Message {
+    /// Transfer ITS tokens between different chains
     InterchainTransfer {
+        /// The unique identifier of the token being transferred
         token_id: TokenId,
+        /// The address that called the ITS contract on the source chain
         source_address: HexBinary,
+        /// The address that the token will be sent to on the destination chain
+        /// If data is not empty, this address will given the token and executed as a contract on the destination chain
         destination_address: HexBinary,
+        /// The amount of tokens to transfer
         amount: Uint256,
+        /// An optional payload to be provided to the destination address, if `data` is not empty
         data: HexBinary,
     },
+    /// Deploy a new interchain token on the destination chain
     DeployInterchainToken {
+        /// The unique identifier of the token to be deployed
         token_id: TokenId,
+        /// The name of the token
         name: String,
+        /// The symbol of the token
         symbol: String,
+        /// The number of decimal places the token supports
         decimals: u8,
+        /// The address that will be the initial minter of the token (in addition to the ITS contract)
         minter: HexBinary,
     },
+    /// Deploy a new token manager on the destination chain
     DeployTokenManager {
+        /// The unique identifier of the token that the token manager will manage
         token_id: TokenId,
+        /// The type of token manager to deploy
         token_manager_type: TokenManagerType,
+        /// The parameters to be provided to the token manager contract
         params: HexBinary,
     },
 }
 
-/// ITS message type that can be sent between ITS edge contracts and the ITS Hub
+/// A message sent between ITS edge contracts and the ITS hub contract (defined in this crate).
+/// `HubMessage` is used to route an ITS [`Message`] between ITS edge contracts on different chains via the ITS Hub.
 #[cw_serde]
 #[derive(Eq)]
-pub enum ItsHubMessage {
+pub enum HubMessage {
     /// ITS edge source contract -> ITS Hub
     SendToHub {
         /// True destination chain of the ITS message
-        destination_chain: ChainName,
-        message: ItsMessage,
+        destination_chain: ChainNameRaw,
+        message: Message,
     },
     /// ITS Hub -> ITS edge destination contract
     ReceiveFromHub {
         /// True source chain of the ITS message
-        source_chain: ChainName,
-        message: ItsMessage,
+        source_chain: ChainNameRaw,
+        message: Message,
     },
+}
+
+impl HubMessage {
+    pub fn message(&self) -> &Message {
+        match self {
+            HubMessage::SendToHub { message, .. } => message,
+            HubMessage::ReceiveFromHub { message, .. } => message,
+        }
+    }
 }
 
 impl TokenId {
---
+++ interchain-token-service/tests/utils/params.rs
@@ -0,0 +1,5 @@
+pub const AXELARNET: &str = "axelarnet";
+pub const ROUTER: &str = "router";
+pub const GATEWAY: &str = "gateway";
+pub const GOVERNANCE: &str = "governance";
+pub const ADMIN: &str = "admin";
