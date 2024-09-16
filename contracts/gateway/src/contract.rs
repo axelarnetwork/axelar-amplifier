cc2507ee2875dc1c014d0cde523df2ec36f7d1e0
Christian Gorenflo <christian@axelar.network>
2024-09-16 17:31:38 -0400

    refactor(contracts): re-export gateway and router messages

--- contracts/gateway/src/contract.rs
+++ contracts/gateway/src/contract.rs
@@ -5,10 +5,9 @@ use axelar_wasm_std::{address, FnExt};
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response};
 use error_stack::ResultExt;
-use gateway_api::msg::{ExecuteMsg, QueryMsg};
 use router_api::client::Router;
 
-use crate::msg::InstantiateMsg;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::state;
 use crate::state::Config;
 
