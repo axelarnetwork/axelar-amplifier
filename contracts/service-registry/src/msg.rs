2ef52431c8d89d9f762c406dd54c94031d0d598a
Christian Gorenflo <christian@axelar.network>
2024-09-27 15:08:53 -0400

    refactor(service-registry): remove unused MigrationMsg (#641)

--- contracts/service-registry/src/msg.rs
+++ contracts/service-registry/src/msg.rs
@@ -1,14 +1,8 @@
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::Addr;
 
 #[cw_serde]
 pub struct InstantiateMsg {
     pub governance_account: String,
-}
-
-#[cw_serde]
-pub struct MigrateMsg {
-    pub coordinator_contract: Addr,
 }
 
 // these messages and structs are extracted into a separate package to avoid circular dependencies
