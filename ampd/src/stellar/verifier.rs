a8149418b993f20dbb7850f9e061aabee434e708
ahramy <ahram@interoplabs.io>
2024-10-20 20:46:54 -0700

    update verifier.rs

--- ampd/src/stellar/verifier.rs
+++ ampd/src/stellar/verifier.rs
@@ -365,7 +365,7 @@ mod test {
             topics: vec![
                 ScVal::Symbol(ScSymbol(StringM::from_str(TOPIC_ROTATED).unwrap())),
                 ScVal::U64(1),
-                ScVal::Bytes(ScBytes(weighted_signers_hash)),
+                ScVal::Bytes(ScBytes(BytesM::try_from(weighted_signers_hash).unwrap())),
             ]
             .try_into()
             .unwrap(),
