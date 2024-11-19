81c5bd9d3de5177fcd50f35010ec94ffd453ccec
Milap Sheth <milap@interoplabs.io>
2024-11-19 13:51:28 -0500

    fix(ampd): bump ampd patch version (#704)

--- contracts/multisig-prover/src/contract.rs
+++ contracts/multisig-prover/src/contract.rs
@@ -19,7 +19,7 @@ pub const START_MULTISIG_REPLY_ID: u64 = 1;
 
 const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-const BASE_VERSION: &str = "1.0.0";
+const BASE_VERSION: &str = "1.1.0";
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
--- contracts/rewards/src/contract.rs
+++ contracts/rewards/src/contract.rs
@@ -18,7 +18,7 @@ mod query;
 
 const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-const BASE_VERSION: &str = "1.1.0";
+const BASE_VERSION: &str = "1.2.0";
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(
