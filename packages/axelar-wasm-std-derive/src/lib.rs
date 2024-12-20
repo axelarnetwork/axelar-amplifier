e4ddfb2638adaa7ff3fb79385aab0bf6c3c66162
Sammy <liuzhihao1991@gmail.com>
2024-12-20 18:47:34 -0500

    feat(minor-axelar-wasm-std-derive): derive macro into_event_derive

--- packages/axelar-wasm-std-derive/src/lib.rs
+++ packages/axelar-wasm-std-derive/src/lib.rs
@@ -20,3 +20,37 @@ pub fn into_contract_error_derive(input: TokenStream) -> TokenStream {
 
     gen.into()
 }
+
+#[proc_macro_attribute]
+pub fn into_event_derive(arg: TokenStream, input: TokenStream) -> TokenStream {
+    let event_name = syn::parse_macro_input!(arg as syn::LitStr);
+    let input = syn::parse_macro_input!(input as syn::ItemStruct);
+
+    let event_struct = input.ident.clone();
+
+    TokenStream::from(quote! {
+        #input
+
+        impl From<&#event_struct> for cosmwasm_std::Event {
+            fn from(event: &#event_struct) -> Self {
+                let attributes: Vec<_> = serde_json::to_value(event)
+                    .expect("failed to serialize event")
+                    .as_object()
+                    .expect("event must be a json object")
+                    .into_iter()
+                    .map(|(key, value)| {
+                        cosmwasm_std::Attribute::new(key, value.to_string())
+                    })
+                    .collect();
+
+                cosmwasm_std::Event::new(#event_name.to_string()).add_attributes(attributes)
+            }
+        }
+
+        impl From<#event_struct> for cosmwasm_std::Event {
+            fn from(event: #event_struct) -> Self {
+                (&event).into()
+            }
+        }
+    })
+}
