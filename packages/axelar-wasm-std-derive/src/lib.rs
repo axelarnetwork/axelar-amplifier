12d76bc53e27ed433a896d947f4723532c8547e5
Sammy <liuzhihao1991@gmail.com>
2025-01-07 01:09:52 -0500

    feat(axelar-wasm-std-minor): derive macro into-event support generics

--- packages/axelar-wasm-std-derive/src/lib.rs
+++ packages/axelar-wasm-std-derive/src/lib.rs
@@ -5,7 +5,7 @@ use itertools::Itertools;
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span, TokenStream as TokenStream2};
 use quote::quote;
-use syn::{DeriveInput, FieldsNamed, ItemEnum, Variant};
+use syn::{DeriveInput, FieldsNamed, Generics, ItemEnum, Variant};
 
 #[proc_macro_derive(IntoContractError)]
 pub fn into_contract_error_derive(input: TokenStream) -> TokenStream {
@@ -33,6 +33,7 @@ pub fn into_contract_error_derive(input: TokenStream) -> TokenStream {
 /// ```
 /// use std::collections::BTreeMap;
 /// use serde::Serialize;
+/// use cosmwasm_std::{Empty, Event};
 ///
 /// use axelar_wasm_std_derive::IntoEvent;
 ///
@@ -45,7 +46,10 @@ pub fn into_contract_error_derive(input: TokenStream) -> TokenStream {
 /// }
 ///
 /// #[derive(IntoEvent)]
-/// enum SomeEvents {
+/// enum SomeEvents<T, U> where
+/// T: Serialize,
+/// U: Serialize
+/// {
 ///     SomeEmptyEvent,
 ///     SomeOtherEmptyEvent {},
 ///     SomeEvent {
@@ -54,17 +58,21 @@ pub fn into_contract_error_derive(input: TokenStream) -> TokenStream {
 ///         some_bool: bool,
 ///         some_object: SomeObject,
 ///     },
+///     SomeGenericsEvent {
+///         some_generics: T,
+///         some_other_generics: U,
+///     }
 /// }
 ///
-/// let actual = cosmwasm_std::Event::from(SomeEvents::SomeEmptyEvent);
-/// let expected = cosmwasm_std::Event::new("some_empty_event");
+/// let actual = Event::from(SomeEvents::SomeEmptyEvent::<Empty, Empty>);
+/// let expected = Event::new("some_empty_event");
 /// assert_eq!(actual, expected);
 ///
-/// let actual = cosmwasm_std::Event::from(SomeEvents::SomeOtherEmptyEvent {});
-/// let expected = cosmwasm_std::Event::new("some_other_empty_event");
+/// let actual = Event::from(SomeEvents::SomeOtherEmptyEvent::<Empty, Empty> {});
+/// let expected = Event::new("some_other_empty_event");
 /// assert_eq!(actual, expected);
 ///
-/// let actual = cosmwasm_std::Event::from(SomeEvents::SomeEvent {
+/// let actual = Event::from(SomeEvents::SomeEvent::<Empty, Empty> {
 ///     some_uint: 42,
 ///     some_string: "some string".to_string(),
 ///     some_bool: true,
@@ -75,11 +83,20 @@ pub fn into_contract_error_derive(input: TokenStream) -> TokenStream {
 ///         some_map: [("a".to_string(), "b".to_string()), ("c".to_string(), "d".to_string()), ("e".to_string(), "f".to_string())].into_iter().collect(),
 ///     },
 /// });
-/// let expected = cosmwasm_std::Event::new("some_event")
+/// let expected = Event::new("some_event")
 ///     .add_attribute("some_uint", "42")
 ///     .add_attribute("some_string", "\"some string\"")
 ///     .add_attribute("some_bool", "true")
 ///     .add_attribute("some_object", "{\"some_option\":\"some option\",\"some_other_option\":null,\"some_vec\":[\"a\",\"b\"],\"some_map\":{\"a\":\"b\",\"c\":\"d\",\"e\":\"f\"}}");
+/// assert_eq!(actual, expected);
+///
+/// let actual = Event::from(SomeEvents::SomeGenericsEvent::<String, u64> {
+///     some_generics: "some generics".to_string(),
+///     some_other_generics: 42,
+/// });
+/// let expected = Event::new("some_generics_event")
+///     .add_attribute("some_generics", "\"some generics\"")
+///     .add_attribute("some_other_generics", "42");
 /// assert_eq!(actual, expected);
 /// ```
 ///
@@ -102,15 +119,22 @@ pub fn into_contract_error_derive(input: TokenStream) -> TokenStream {
 /// #     Unnamed(u64),
 /// # }
 /// ```
+/// ```compile_fail
+/// # use axelar_wasm_std_derive::IntoEvent;
+///
+/// # #[derive(IntoEvent)] // should not compile because the event enum has no variants
+/// # enum SomeEmptyEvent {}
+/// ```
 #[proc_macro_derive(IntoEvent)]
 pub fn into_event(input: TokenStream) -> TokenStream {
     let ItemEnum {
         variants,
         ident: event_enum,
+        generics,
         ..
     } = syn::parse_macro_input!(input as syn::ItemEnum);
 
-    try_into_event(event_enum, variants)
+    try_into_event(event_enum, variants, generics)
         .unwrap_or_else(syn::Error::into_compile_error)
         .into()
 }
@@ -118,6 +142,7 @@ pub fn into_event(input: TokenStream) -> TokenStream {
 fn try_into_event(
     event_enum: Ident,
     variants: impl IntoIterator<Item = Variant>,
+    generics: Generics,
 ) -> Result<TokenStream2, syn::Error> {
     let variant_matches: Vec<_> = variants
         .into_iter()
@@ -134,18 +159,23 @@ fn try_into_event(
             )),
         })
         .try_collect()?;
+    if variant_matches.is_empty() {
+        return Err(syn::Error::new(Span::call_site(), "no variants found"));
+    }
+
+    let (impl_generics, type_generics, where_clause) = generics.split_for_impl();
 
     Ok(quote! {
-        impl From<&#event_enum> for cosmwasm_std::Event {
-            fn from(event: &#event_enum) -> Self {
+        impl #impl_generics From<&#event_enum #type_generics> for cosmwasm_std::Event #where_clause {
+            fn from(event: &#event_enum #type_generics) -> Self {
                 match event {
                     #(#variant_matches),*
                 }
             }
         }
 
-        impl From<#event_enum> for cosmwasm_std::Event {
-            fn from(event: #event_enum) -> Self {
+        impl #impl_generics From<#event_enum #type_generics> for cosmwasm_std::Event #where_clause {
+            fn from(event: #event_enum #type_generics) -> Self {
                 (&event).into()
             }
         }
