988b7b0234659ba280b5013ddff1a7439a2b0c2b
Edwin Guajardo <edwin@axelar.network>
2025-02-05 11:18:33 -0600

    cleanup

--- tools/migration-remover/src/main.rs
+++ tools/migration-remover/src/main.rs
@@ -1,11 +1,21 @@
 use clap::{arg, Parser};
 use error_stack::{Result, ResultExt};
-use std::{env, fs, path::Path};
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
 use thiserror::Error;
+use toml::Table;
 
 #[derive(Error, Debug)]
-#[error("failed to remove migration code")]
-struct Error;
+enum Error {
+    #[error("failed to parse contract's Cargo.toml")]
+    Cargo,
+    #[error("failed to read template file")]
+    Template,
+    #[error("failed to reset contract's 'migrations' module")]
+    MigrationMod,
+}
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -17,20 +27,59 @@ struct Args {
 const CONTRACTS_RELATIVE_PATH: &str = "../../contracts/";
 const TEMPLATE_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/src/template.rs");
 
+fn next_migrate_version_req(cargo_toml_path: &PathBuf) -> Result<String, Error> {
+    let content = fs::read_to_string(cargo_toml_path).change_context(Error::Cargo)?;
+    let parsed_toml = content.parse::<Table>().change_context(Error::Cargo)?;
+
+    let version = parsed_toml
+        .get("package")
+        .and_then(|package| package.get("version"))
+        .and_then(|version| version.as_str())
+        .ok_or(Error::Cargo)?;
+
+    Ok(version.split(".").take(2).collect::<Vec<_>>().join("."))
+}
+
+fn read_template(contract_root_dir: &PathBuf) -> Result<String, Error> {
+    let from_version = next_migrate_version_req(&contract_root_dir.join("Cargo.toml"))?;
+
+    let template = fs::read_to_string(TEMPLATE_PATH)
+        .change_context(Error::Template)?
+        .replace(
+            r#"#[migrate_from_version("0.0")]"#,
+            &format!(r#"#[migrate_from_version("{}")]"#, from_version),
+        );
+
+    Ok(template)
+}
+
+fn reset_migration_mod(contract_root_dir: &PathBuf, content: &str) -> Result<(), Error> {
+    let migrations_mod_dir = contract_root_dir.join("src/contract/migrations");
+
+    fs::remove_dir_all(&migrations_mod_dir)
+        .and_then(|_| fs::create_dir(&migrations_mod_dir))
+        .and_then(|_| fs::write(&migrations_mod_dir.join("mod.rs"), content))
+        .change_context(Error::MigrationMod)
+}
+
 fn main() -> Result<(), Error> {
     let args = Args::parse();
-    println!("Removing migration code from {} contract", args.contract);
 
-    let migrations_mod_dir = Path::new(env!("CARGO_MANIFEST_DIR"))
+    let contract_root_dir = Path::new(env!("CARGO_MANIFEST_DIR"))
         .join(CONTRACTS_RELATIVE_PATH)
-        .join(&args.contract)
-        .join("src/contract/migrations");
+        .join(&args.contract);
 
-    fs::remove_dir_all(&migrations_mod_dir).change_context(Error)?;
-    fs::create_dir(&migrations_mod_dir).change_context(Error)?;
+    read_template(&contract_root_dir)
+        .and_then(|template| reset_migration_mod(&contract_root_dir, &template))?;
 
-    let template = fs::read_to_string(TEMPLATE_PATH).change_context(Error)?;
-    fs::write(&migrations_mod_dir.join("mod.rs"), template).change_context(Error)?;
+    println!(
+        "Migration code removed successfully from contract `{}`",
+        args.contract
+    );
 
     Ok(())
 }
+
+// tests that `template.rs` compiles correctly
+#[cfg(test)]
+mod template;
