740b6faa989ec4a2a8f8d5204f2cb0921f3297dc
Sammy <liuzhihao1991@gmail.com>
2025-03-20 13:58:26 -0400

    refactor(minor-ampd): cleanup all previous work of ampd grpc server and rename the proto folder

--- ampd/build.rs
+++ ampd/build.rs
@@ -1,19 +1,15 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(false)
-        .compile(&["proto/tofnd/multisig.proto"], &["proto/tofnd"])?;
-
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(true)
-        .compile(&["proto/ampd.proto"], &["proto"])?;
+    tonic_build::configure().build_server(false).compile(
+        &["internal-proto/tofnd/multisig.proto"],
+        &["internal-proto/tofnd"],
+    )?;
 
     tonic_build::configure()
         .build_server(false)
         .build_client(false)
         .compile(
-            &["proto/axelar/auxiliary/v1beta1/tx.proto"],
-            &["proto", "proto/third_party"],
+            &["internal-proto/axelar/auxiliary/v1beta1/tx.proto"],
+            &["internal-proto", "internal-proto/third_party"],
         )?;
 
     Ok(())
