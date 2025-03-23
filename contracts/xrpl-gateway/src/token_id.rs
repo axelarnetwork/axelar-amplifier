8df950ee75b8919be1ced714d1c801d3dd1fb9ae
k4m4 <nikolaskam@gmail.com>
2025-03-23 15:41:59 -0400

    test(xrpl-gateway): add goldie unit tests for token_id module

--- contracts/xrpl-gateway/src/token_id.rs
+++ contracts/xrpl-gateway/src/token_id.rs
@@ -44,3 +44,53 @@ pub fn chain_name_hash(chain_name: ChainName) -> [u8; 32] {
 pub fn currency_hash(currency: &XRPLCurrency) -> [u8; 32] {
     Keccak256::digest(currency.as_bytes()).into()
 }
+
+#[cfg(test)]
+mod test {
+    use std::str::FromStr;
+
+    use cosmwasm_std::HexBinary;
+    use router_api::ChainName;
+    use xrpl_types::types::{XRPLAccountId, XRPLCurrency};
+
+    #[test]
+    pub fn token_id() {
+        let salt = [0u8; 32];
+        let token_id = super::token_id(salt);
+        goldie::assert!(token_id.to_string());
+    }
+
+    #[test]
+    pub fn linked_token_deploy_salt() {
+        let chain_name_hash = [0u8; 32];
+        let deployer = &XRPLAccountId::from([0u8; 20]);
+        let salt = [0u8; 32];
+
+        let deploy_salt = super::linked_token_deploy_salt(chain_name_hash, deployer, salt);
+        goldie::assert!(HexBinary::from(deploy_salt).to_string());
+    }
+
+    #[test]
+    pub fn linked_token_id() {
+        let chain_name_hash = [0u8; 32];
+        let deployer = &XRPLAccountId::from([0u8; 20]);
+        let salt = [0u8; 32];
+
+        let token_id = super::linked_token_id(chain_name_hash, deployer, salt);
+        goldie::assert!(token_id.to_string());
+    }
+
+    #[test]
+    pub fn chain_name_hash() {
+        let chain_name = ChainName::from_str("xrpl").unwrap();
+        let chain_name_hash = super::chain_name_hash(chain_name);
+        goldie::assert!(HexBinary::from(chain_name_hash).to_string());
+    }
+
+    #[test]
+    pub fn currency_hash() {
+        let currency = XRPLCurrency::XRP;
+        let currency_hash = super::currency_hash(&currency);
+        goldie::assert!(HexBinary::from(currency_hash).to_string());
+    }
+}
