dd176ce0c9550e91a7487dda919ff727cd40dab2
maancham <houmaan@interoplabs.io>
2025-03-27 12:49:44 -0400

    feat: add dummy test for ampd-handlers

--- ampd-handlers/src/main.rs
+++ ampd-handlers/src/main.rs
@@ -1,3 +1,17 @@
+fn get_greeting() -> &'static str {
+    "Hello from AMPD handler!"
+}
+
 fn main() {
-    println!("Hello from AMPD handler!");
+    println!("{}", get_greeting());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_greeting() {
+        assert_eq!(get_greeting(), "Hello from AMPD handler!");
+    }
 }
