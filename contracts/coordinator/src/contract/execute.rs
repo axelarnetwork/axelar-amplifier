2398a844f2c22c76384ab6235cf263a0d21708dc
Houmaan Chamani <houmaan@interoplabs.io>
2025-04-07 14:31:43 -0400

    feat(coordinator): add RegisterChain execute endpoint (#775)

--- contracts/coordinator/src/contract/execute.rs
+++ contracts/coordinator/src/contract/execute.rs
@@ -4,7 +4,7 @@ use cosmwasm_std::{Addr, DepsMut, MessageInfo, Response};
 use router_api::ChainName;
 
 use crate::error::ContractError;
-use crate::state::{save_prover_for_chain, update_verifier_set_for_prover};
+use crate::state::{save_chain_contracts, save_prover_for_chain, update_verifier_set_for_prover};
 
 pub fn register_prover(
     deps: DepsMut,
@@ -12,6 +12,23 @@ pub fn register_prover(
     new_prover_addr: Addr,
 ) -> Result<Response, ContractError> {
     save_prover_for_chain(deps.storage, chain_name, new_prover_addr)?;
+    Ok(Response::new())
+}
+
+pub fn register_chain(
+    deps: DepsMut,
+    chain_name: ChainName,
+    prover_addr: Addr,
+    gateway_addr: Addr,
+    voting_verifier_address: Addr,
+) -> Result<Response, ContractError> {
+    save_chain_contracts(
+        deps.storage,
+        chain_name,
+        prover_addr,
+        gateway_addr,
+        voting_verifier_address,
+    )?;
     Ok(Response::new())
 }
 
