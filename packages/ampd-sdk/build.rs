e4640a75dc0a6f2e38f230335fa27220cf409aec
maancham <houmaan@interoplabs.io>
2025-04-07 16:41:34 -0400

    fix: bump up prost and tonic versions

--- packages/ampd-sdk/build.rs
+++ packages/ampd-sdk/build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .build_client(true)
-        .compile(&["ampd-proto/ampd/v1/ampd.proto"], &["ampd-proto/ampd/v1"])?;
+        .compile_protos(&["ampd-proto/ampd/v1/ampd.proto"], &["ampd-proto/ampd/v1"])?;
 
     Ok(())
 }
