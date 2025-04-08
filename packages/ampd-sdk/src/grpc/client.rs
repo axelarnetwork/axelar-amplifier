90d53dcdc2aa31bf6446a2006a23637a52d81e91
maancham <houmaan@interoplabs.io>
2025-04-08 15:52:56 -0400

    chore: rename client and rpc impl, use less generic errors

--- packages/ampd-sdk/src/grpc/client.rs
+++ packages/ampd-sdk/src/grpc/client.rs
@@ -2,7 +2,7 @@ use async_trait::async_trait;
 use error_stack::{Report, Result};
 use mockall::automock;
 use thiserror::Error;
-use tonic::{codegen, transport, Streaming};
+use tonic::{transport, Streaming};
 
 use super::proto::blockchain_service_client::BlockchainServiceClient;
 use super::proto::crypto_service_client::CryptoServiceClient;
@@ -20,8 +20,8 @@ pub enum Error {
 #[automock]
 #[async_trait]
 #[allow(dead_code)]
-pub trait GRPCClient {
-    // TODO: This trait's methods should return our own types raher than the generated protobuf ones
+pub trait Client {
+    // TODO: This trait's methods should return our own types rather than the generated protobuf ones
     async fn subscribe(
         &self,
         request: SubscribeRequest,
@@ -29,32 +29,32 @@ pub trait GRPCClient {
 }
 
 #[allow(dead_code)]
-pub struct Client {
+pub struct GRPCClient {
     pub blockchain: BlockchainServiceClient<transport::Channel>,
     pub crypto: CryptoServiceClient<transport::Channel>,
 }
 
 #[allow(dead_code)]
-pub async fn new<D>(dst: D) -> Result<Client, Error>
-where
-    D: TryInto<transport::Endpoint>,
-    D::Error: Into<codegen::StdError>,
-{
-    let conn = transport::Endpoint::new(dst)
-        .map_err(|e| Report::new(Error::GRpcConnection(e)))?
+pub async fn new(url: &str) -> Result<GRPCClient, Error> {
+    let endpoint = transport::Endpoint::from_shared(url.to_string())
+        .map_err(Into::into) // Convert to Error::GRpcConnection via #[from]
+        .map_err(Report::new)?;
+
+    let conn = endpoint
         .connect()
         .await
-        .map_err(|e| Report::new(Error::GRpcConnection(e)))?;
+        .map_err(Into::into) // Convert to Error::GRpcConnection via #[from]
+        .map_err(Report::new)?;
 
     let blockchain = BlockchainServiceClient::new(conn.clone());
     let crypto = CryptoServiceClient::new(conn);
 
-    Ok(Client { blockchain, crypto })
+    Ok(GRPCClient { blockchain, crypto })
 }
 
 #[async_trait]
 #[allow(clippy::todo)]
-impl GRPCClient for Client {
+impl Client for GRPCClient {
     async fn subscribe(
         &self,
         _request: SubscribeRequest,
@@ -69,7 +69,7 @@ mod tests {
 
     #[test]
     fn test_mock_client() {
-        let mut _mock = MockGRPCClient::new();
+        let mut _mock = MockClient::new();
         // This test just verifies the mock can be created
     }
 }
