a5f60048235a806aad44e9ac30778192933bb1f0
k4m4 <nikolaskam@gmail.com>
2025-04-17 23:49:53 +0900

    chore(xrpl-multisig-prover): bump BASE_VERSION to v1.1.1

--- contracts/xrpl-multisig-prover/src/contract.rs
+++ contracts/xrpl-multisig-prover/src/contract.rs
@@ -22,7 +22,7 @@ pub const START_MULTISIG_REPLY_ID: u64 = 1;
 
 const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-const BASE_VERSION: &str = "1.0.0";
+const BASE_VERSION: &str = "1.1.1";
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
