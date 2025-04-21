3f036ec2bb4bf9c3c420617e501d6637bb96c88c
Sammy <liuzhihao1991@gmail.com>
2025-04-21 18:16:25 -0400

    feat(minor-ampd): add timeout to tofnd and cosmos grpc clients (#810)

--- ampd/src/tofnd/error.rs
+++ ampd/src/tofnd/error.rs
@@ -2,8 +2,10 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("grpc failed")]
-    Grpc,
+    #[error("failed to connect to the grpc endpoint")]
+    GrpcConnection(#[from] tonic::transport::Error),
+    #[error("failed to make the grpc request")]
+    GrpcRequest(#[from] tonic::Status),
     #[error("keygen failed")]
     KeygenFailed,
     #[error("sign failed")]
