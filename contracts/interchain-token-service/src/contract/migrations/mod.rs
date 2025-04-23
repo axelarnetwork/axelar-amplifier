2d916358d3f2aea47b1f4198050dc81a0c532736
CJ Cobb <46455409+cjcobb23@users.noreply.github.com>
2025-04-23 14:39:04 -0600

    fix(interchain-token-service): fix migration to handle all values (#812)

--- contracts/interchain-token-service/src/contract/migrations/mod.rs
+++ contracts/interchain-token-service/src/contract/migrations/mod.rs
@@ -5,7 +5,7 @@ use cosmwasm_schema::cw_serde;
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{DepsMut, Env, Response};
 
-pub mod v1_1_0;
+pub mod v1_1_1;
 
 #[cw_serde]
 pub struct MigrateMsg {
@@ -15,6 +15,6 @@ pub struct MigrateMsg {
 #[cfg_attr(not(feature = "library"), entry_point)]
 #[migrate_from_version("1.1")]
 pub fn migrate(deps: DepsMut, _env: Env, msg: MigrateMsg) -> Result<Response, ContractError> {
-    v1_1_0::migrate(deps, msg)?;
+    v1_1_1::migrate(deps, msg)?;
     Ok(Response::default())
 }
