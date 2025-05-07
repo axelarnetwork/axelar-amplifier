632fa6f8e2621a415e820cbe71911989e51848f4
maancham <houmaan@interoplabs.io>
2025-05-07 16:25:41 -0400

    fix: add protobuf installation and run fmt

--- ampd-handlers/src/main.rs
+++ ampd-handlers/src/main.rs
@@ -1,21 +1,20 @@
-use ampd_sdk::grpc::client::{new as new_client, Client};
-use tokio_stream::StreamExt;
 use std::env;
 
+use ampd_sdk::grpc::client::{new as new_client, Client};
+use tokio_stream::StreamExt;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ampd_url = env::var("AMPD_SERVICE_URL").unwrap_or_else(|_| "http://127.0.0.1:9090".to_string());
+    let ampd_url =
+        env::var("AMPD_SERVICE_URL").unwrap_or_else(|_| "http://127.0.0.1:9090".to_string());
 
     println!("Attempting to connect to AMPD server at {}", ampd_url);
     let mut client = new_client(&ampd_url).await?;
 
     println!("Connected to AMPD server at {}", ampd_url);
 
-    let mut event_stream = client
-        .subscribe(vec![], true)
-        .await?;
-    
+    let mut event_stream = client.subscribe(vec![], true).await?;
+
     println!("Subscribed to events. Listening...");
 
     while let Some(event_result) = event_stream.next().await {
