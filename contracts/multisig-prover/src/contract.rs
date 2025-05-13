b0facac5d4c42f24b3177f10e972dc01376f41c0
SDavidson1177 <solomon@interoplabs.io>
2025-05-13 11:34:17 -0400

    refactor: create multisig-prover and voting-verifier api (#835)

--- contracts/multisig-prover/src/contract.rs
+++ contracts/multisig-prover/src/contract.rs
@@ -128,6 +128,7 @@ mod tests {
     };
     use multisig::msg::Signer;
     use multisig::verifier_set::VerifierSet;
+    use multisig_prover_api::encoding::Encoder;
     use prost::Message;
     use router_api::CrossChainId;
 
@@ -139,7 +140,6 @@ mod tests {
         mock_querier_handler, ADMIN, COORDINATOR_ADDRESS, GATEWAY_ADDRESS, GOVERNANCE,
         MULTISIG_ADDRESS, SERVICE_NAME, SERVICE_REGISTRY_ADDRESS, VOTING_VERIFIER_ADDRESS,
     };
-    use crate::Encoder;
 
     const RELAYER: &str = "relayer";
     const MULTISIG_SESSION_ID: Uint64 = Uint64::one();
