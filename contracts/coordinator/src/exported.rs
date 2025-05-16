945fae8085ea03a78e397ad9cd43bad04dccce9c
Yidan Wang <Yidan@interoplabs.io>
2025-05-16 10:26:52 -0400

    refactor(coordinator): 7329 add exported module

---
+++ contracts/coordinator/src/exported.rs
@@ -0,0 +1,5 @@
+use cosmwasm_std::Addr;
+pub use router_api::ChainName;
+pub type ProverAddress = Addr;
+pub type GatewayAddress = Addr;
+pub type VerifierAddress = Addr;
--- contracts/coordinator/src/lib.rs
+++ contracts/coordinator/src/lib.rs
@@ -4,3 +4,6 @@ pub use client::Client;
 pub mod contract;
 pub mod msg;
 mod state;
+
+mod exported;
+pub use exported::*;
