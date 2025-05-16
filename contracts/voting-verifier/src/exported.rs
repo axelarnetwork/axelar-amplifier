160066682a2839bd2e6c75e78ec189b86f542f48
Yidan Wang <Yidan@interoplabs.io>
2025-05-16 19:40:44 -0400

    add exported.rs

---
+++ contracts/voting-verifier/src/exported.rs
@@ -0,0 +1,3 @@
+
+pub use crate::client::Client;
+pub use crate::msg::{MessageStatus,QueryMsg};
--- contracts/voting-verifier/src/lib.rs
+++ contracts/voting-verifier/src/lib.rs
@@ -1,8 +1,8 @@
 mod client;
-pub use client::Client;
-
 pub mod contract;
-pub mod error;
+mod error;
 pub mod events;
 pub mod msg;
 mod state;
+mod exported;
+pub use exported::*;
