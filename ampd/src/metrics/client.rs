316a525bb3ad8bd58ee53dfa5e20be2919807c1c
Yidan Wang <Yidan@interoplabs.io>
2025-05-22 00:07:58 -0400

    feature(prometheus-client): add a rough draft of prometheus client

---
+++ ampd/src/metrics/client.rs
@@ -0,0 +1,27 @@
+use tokio::sync::mpsc::Sender;
+
+use crate::metrics::msg::{MetricsError, MetricsMsg};
+
+#[derive(Clone)]
+pub struct MetricsClient {
+    sender: Sender<MetricsMsg>,
+}
+
+impl MetricsClient {
+    pub fn new(sender: Sender<MetricsMsg>) -> Self {
+        Self { sender }
+    }
+
+    pub fn inc_block_received(&self) -> Result<(), MetricsError> {
+        self.sender
+            .try_send(MetricsMsg::IncBlockReceived)
+            .map_err(|_| MetricsError::MetricUpdateFailed)?;
+        Ok(())
+    }
+    pub fn inc_timer(&self) -> Result<(), MetricsError> {
+        self.sender
+            .try_send(MetricsMsg::IncTimer)
+            .map_err(|_| MetricsError::MetricUpdateFailed)?;
+        Ok(())
+    }
+}
