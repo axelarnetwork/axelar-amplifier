2153e24fcb2a379163b6cec334cdcd6778d7508b
Yidan Wang <Yidan@interoplabs.io>
2025-05-23 09:52:12 -0400

    refactor(service-registry): update modurality

--- contracts/service-registry/src/lib.rs
+++ contracts/service-registry/src/lib.rs
@@ -1,9 +1,9 @@
 pub mod contract;
+mod exported;
 pub mod helpers;
 pub mod msg;
 mod state;
-
+pub use exported::*;
 pub use service_registry_api::{
     AuthorizationState, BondingState, Service, Verifier, WeightedVerifier,
 };
-pub use state::VERIFIER_WEIGHT;
