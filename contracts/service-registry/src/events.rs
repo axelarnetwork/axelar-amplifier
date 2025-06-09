6ad40bbaf6935e87c49771c19845babbe8291c1a
Yidan Wang <Yidan@interoplabs.io>
2025-06-09 00:47:35 -0400

    add goldie event

--- contracts/service-registry/src/events.rs
+++ contracts/service-registry/src/events.rs
@@ -15,3 +15,32 @@ pub enum Event {
         chains: Vec<ChainName>,
     },
 }
+
+#[cfg(test)]
+mod test {
+    use cosmwasm_std::{Addr, Event as CosmwasmEvent};
+
+    use super::Event;
+
+    #[test]
+    fn chains_support_registered_is_serializable() {
+        let event = Event::ChainsSupportRegistered {
+            verifier: Addr::unchecked("verifier"),
+            service_name: "test_service".to_string(),
+            chains: vec!["ethereum".parse().unwrap(), "polygon".parse().unwrap()],
+        };
+        let cosmwasm_event: CosmwasmEvent = event.into();
+        goldie::assert_json!(cosmwasm_event);
+    }
+
+    #[test]
+    fn chains_support_deregistered_is_serializable() {
+        let event = Event::ChainsSupportDeregistered {
+            verifier: Addr::unchecked("verifier"),
+            service_name: "test_service".to_string(),
+            chains: vec!["ethereum".parse().unwrap(), "polygon".parse().unwrap()],
+        };
+        let cosmwasm_event: CosmwasmEvent = event.into();
+        goldie::assert_json!(cosmwasm_event);
+    }
+}
