a49016b88f0ee552c2d840c3d511a9bbb5aa5972
maancham <houmaan@interoplabs.io>
2025-06-23 14:53:34 -0400

    chore: sync with unit tests

--- packages/ampd-sdk/src/grpc/client.rs
+++ packages/ampd-sdk/src/grpc/client.rs
@@ -377,6 +377,7 @@ mod tests {
     use tonic::{Request, Response, Status};
 
     use super::*;
+    use crate::grpc::client::new as new_client;
     use crate::grpc::error::GrpcError;
 
     type ServerSubscribeStream =
@@ -415,7 +416,7 @@ mod tests {
             .add_service(BlockchainServiceServer::new(mock_blockchain))
             .add_service(CryptoServiceServer::new(mock_crypto));
 
-        let addr: std::net::SocketAddr = "[::1]:0".parse().unwrap();
+        let addr: std::net::SocketAddr = "127.0.0.1:0".parse().unwrap();
         let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
         let server_addr = listener.local_addr().unwrap();
         let bound_server =
@@ -423,7 +424,8 @@ mod tests {
 
         tokio::spawn(bound_server);
         let url = format!("http://{}", server_addr);
-        new(&url).await.unwrap()
+
+        new_client(&url).await.unwrap()
     }
 
     #[tokio::test]
