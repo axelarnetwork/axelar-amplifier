a1324cbd56411392bb4636cc0bd6cfe18731a279
Sammy <liuzhihao1991@gmail.com>
2025-07-14 16:05:21 -0400

    refactor(ampd): replace broadcaster with v2 (#836)
    Co-authored-by: Edwin Guajardo <edwin@axelar.network>

--- ampd/src/handlers/multisig.rs
+++ ampd/src/handlers/multisig.rs
@@ -181,7 +181,6 @@ mod test {
 
     use base64::engine::general_purpose::STANDARD;
     use base64::Engine;
-    use cosmrs::proto::cosmos::base::abci::v1beta1::TxResponse;
     use cosmrs::AccountId;
     use cosmwasm_std::{HexBinary, Uint64};
     use error_stack::{Report, Result};
@@ -193,7 +192,6 @@ mod test {
     use tokio::sync::watch;
 
     use super::*;
-    use crate::broadcaster::MockBroadcaster;
     use crate::tofnd;
     use crate::tofnd::MockMultisig;
 
@@ -283,11 +281,6 @@ mod test {
         signer: MockMultisig,
         latest_block_height: u64,
     ) -> Handler<MockMultisig> {
-        let mut broadcaster = MockBroadcaster::new();
-        broadcaster
-            .expect_broadcast()
-            .returning(|_| Ok(TxResponse::default()));
-
         let (_, rx) = watch::channel(latest_block_height);
 
         Handler::new(verifier, multisig, chain, signer, rx)
--- ampd/src/queue/mod.rs
+++
@@ -1,3 +0,0 @@
-mod msg_queue;
-mod proto;
-pub mod queued_broadcaster;
