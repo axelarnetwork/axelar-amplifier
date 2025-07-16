2a072e8c8c6fecefc96b7b071c82e49f6287249f
Yidan Wang <Yidan@interoplabs.io>
2025-07-15 20:49:29 -0400

    all draft

---
+++ ampd/src/handlers/record_metrics.rs
@@ -0,0 +1,29 @@
+use axelar_wasm_std::voting::Vote;
+use tracing::warn;
+
+use crate::monitoring;
+use crate::monitoring::metrics::Msg;
+
+pub fn record_vote_outcome(monitoring_client: &monitoring::Client, vote: &Vote, chain_name: &str) {
+    if let Err(err) = monitoring_client.metrics().record_metric(Msg::VoteOutcome {
+        vote_status: vote.clone(),
+        chain_name: chain_name.to_string(),
+    }) {
+        warn!(error = %err,
+            chain_name = %chain_name,
+            "failed to record vote outcome metrics for vote {:?}", vote);
+    };
+}
+
+pub fn record_vote_processing_failure(monitoring_client: &monitoring::Client, chain_name: &str) {
+    if let Err(err) = monitoring_client
+        .metrics()
+        .record_metric(Msg::VoteProcessingFailure {
+            chain_name: chain_name.to_string(),
+        })
+    {
+        warn!(error = %err,
+             chain_name = %chain_name,
+              "failed to record vote processing failure metrics");
+    };
+}
