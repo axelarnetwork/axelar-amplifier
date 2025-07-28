a5bf5ff7a8c3699c2f0948614fdd95ceb7a2d705
Andrew Jia <andrew@interoplabs.io>
2025-07-28 11:50:44 -0400

    changed  to  and skip all lints in integration-tests

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -1,9 +1,15 @@
+#![cfg_attr(dylint_lib = "amplifier_lints", allow(cosmwasm_addr_in_msg_struct))]
+#![cfg_attr(
+    dylint_lib = "amplifier_lints",
+    allow(execute_without_explicit_permissions)
+)]
+#![cfg_attr(dylint_lib = "amplifier_lints", allow(get_fn))]
 #![cfg_attr(
     dylint_lib = "amplifier_lints",
     allow(msg_without_explicit_permissions)
 )]
-#![cfg_attr(dylint_lib = "amplifier_lints", allow(cosmwasm_addr_in_msg_struct))]
-#![cfg_attr(dylint_lib = "amplifier_lints", allow(warn_on_unwraps))]
+#![cfg_attr(dylint_lib = "amplifier_lints", allow(ref_opt_type))]
+#![cfg_attr(dylint_lib = "amplifier_lints", allow(unwraps_outsisde_tests))]
 
 pub mod contract;
 pub mod coordinator_contract;
