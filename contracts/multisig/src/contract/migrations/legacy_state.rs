30c974ea2a67e1296df67698ab2433045da20299
SDavidson1177 <solomon@interoplabs.io>
2025-07-29 11:15:30 -0400

    Revert "remove test submodule in legacy_state"
    This reverts commit 9ff19246c748a9d253170ef86a2146288f93777a.

--- contracts/multisig/src/contract/migrations/legacy_state.rs
+++ contracts/multisig/src/contract/migrations/legacy_state.rs
@@ -1,18 +1,18 @@
-use axelar_wasm_std::error::ContractError;
-use axelar_wasm_std::{address, nonempty, permission_control};
+use axelar_wasm_std::nonempty;
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Addr, DepsMut, Env, MessageInfo, Response, Storage};
+use cosmwasm_std::{Addr, Storage};
 use cw_storage_plus::Item;
-
-const CONTRACT_NAME: &str = "multisig";
-const CONTRACT_VERSION: &str = "2.1.0";
-
-const CONFIG: Item<Config> = Item::new("config");
 
 #[cw_serde]
 pub struct Config {
     pub rewards_contract: Addr,
     pub block_expiry: nonempty::Uint64, // number of blocks after which a signing session expires
+}
+
+const CONFIG: Item<Config> = Item::new("config");
+
+pub fn load_config(storage: &dyn Storage) -> Result<Config, cosmwasm_std::StdError> {
+    CONFIG.load(storage)
 }
 
 #[cw_serde]
@@ -23,30 +23,37 @@ pub struct InstantiateMsg {
     pub block_expiry: nonempty::Uint64,
 }
 
-pub fn load_config(storage: &dyn Storage) -> Result<Config, cosmwasm_std::StdError> {
-    CONFIG.load(storage)
-}
+#[cfg(test)]
+pub mod test {
+    use axelar_wasm_std::error::ContractError;
+    use axelar_wasm_std::{address, permission_control};
+    use cosmwasm_std::{DepsMut, Env, MessageInfo, Response};
 
-#[allow(dead_code)]
-pub fn instantiate(
-    deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    msg: InstantiateMsg,
-) -> Result<Response, ContractError> {
-    cw2::set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    use super::{Config, InstantiateMsg, CONFIG};
 
-    let admin = address::validate_cosmwasm_address(deps.api, &msg.admin_address)?;
-    let governance = address::validate_cosmwasm_address(deps.api, &msg.governance_address)?;
+    const CONTRACT_NAME: &str = "multisig";
+    const CONTRACT_VERSION: &str = "2.1.0";
 
-    permission_control::set_admin(deps.storage, &admin)?;
-    permission_control::set_governance(deps.storage, &governance)?;
+    pub fn instantiate(
+        deps: DepsMut,
+        _env: Env,
+        _info: MessageInfo,
+        msg: InstantiateMsg,
+    ) -> Result<Response, ContractError> {
+        cw2::set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
-    let config = Config {
-        rewards_contract: address::validate_cosmwasm_address(deps.api, &msg.rewards_address)?,
-        block_expiry: msg.block_expiry,
-    };
-    CONFIG.save(deps.storage, &config)?;
+        let admin = address::validate_cosmwasm_address(deps.api, &msg.admin_address)?;
+        let governance = address::validate_cosmwasm_address(deps.api, &msg.governance_address)?;
 
-    Ok(Response::default())
+        permission_control::set_admin(deps.storage, &admin)?;
+        permission_control::set_governance(deps.storage, &governance)?;
+
+        let config = Config {
+            rewards_contract: address::validate_cosmwasm_address(deps.api, &msg.rewards_address)?,
+            block_expiry: msg.block_expiry,
+        };
+        CONFIG.save(deps.storage, &config)?;
+
+        Ok(Response::default())
+    }
 }
