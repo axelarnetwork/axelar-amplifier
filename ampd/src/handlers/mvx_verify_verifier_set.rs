ccc5ec4f3c306f9d233b2cac095ccf5537a3d33b
Yidan Wang <Yidan@interoplabs.io>
2025-07-30 15:19:41 -0400

    before new handler

--- ampd/src/handlers/mvx_verify_verifier_set.rs
+++ ampd/src/handlers/mvx_verify_verifier_set.rs
@@ -182,7 +182,6 @@ mod tests {
     use hex::ToHex;
     use multisig::key::KeyType;
     use multisig::test::common::{build_verifier_set, ed25519_test_data};
-    use router_api::ChainName;
     use tokio::sync::watch;
     use tokio::test as async_test;
     use voting_verifier::events::{PollMetadata, PollStarted, VerifierSetConfirmation};
