8bdf7e130d0033fa1aede3e75d60b57f0a14d097
Edwin Guajardo <edwin@interoplabs.io>
2025-08-26 16:10:41 -0600

    add unit tests

--- packages/ampd-sdk/src/config.rs
+++ packages/ampd-sdk/src/config.rs
@@ -36,12 +36,12 @@ pub enum Error {
 /// # fn main() -> Result<(), Box<dyn Error>> {
 /// let config = Config::builder()
 ///              .add_file_source("custom_config.toml", true)
-///              .add_env_source("HANDLER")
+///              .add_env_source("MY_HANDLER")
 ///              .build();
 /// # Ok(())
 /// # }
 /// ```
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Config {
     #[serde(deserialize_with = "Url::deserialize_sensitive")]
     pub ampd_url: Url,
@@ -77,14 +77,6 @@ impl ConfigBuilder {
         Self(config::Config::builder())
     }
 
-    /// Adds a source to the config builder.
-    pub fn add_source<T>(self, source: T) -> Self
-    where
-        T: config::Source + Send + Sync + 'static,
-    {
-        Self(self.0.add_source(source))
-    }
-
     /// Adds a file source to the config builder.
     pub fn add_file_source(self, base_file: &str, required: bool) -> Self {
         Self(
@@ -108,5 +100,101 @@ impl ConfigBuilder {
             .build()
             .and_then(|config| config.try_deserialize::<Config>())
             .change_context(Error::Build)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use axelar_wasm_std::assert_err_contains;
+    use std::fs;
+    use std::str::FromStr;
+    use tempfile::TempDir;
+
+    use super::*;
+
+    #[test]
+    fn config_loads_from_default_sources() {
+        let ampd_url = "http://localhost:8080";
+        let chain_name = "some-chain";
+
+        temp_env::with_vars(
+            vec![
+                (format!("{DEFAULT_CONFIG_PREFIX}_AMPD_URL"), Some(ampd_url)),
+                (
+                    format!("{DEFAULT_CONFIG_PREFIX}_CHAIN_NAME"),
+                    Some(chain_name),
+                ),
+            ],
+            || {
+                let config = Config::from_default_sources().unwrap();
+
+                assert_eq!(config.ampd_url, Url::new_sensitive(ampd_url).unwrap());
+                assert_eq!(config.chain_name, ChainName::from_str(chain_name).unwrap());
+            },
+        );
+    }
+
+    #[test]
+    fn config_loads_from_custom_env_vars() {
+        let ampd_url = "http://localhost:8080";
+        let chain_name = "some-chain";
+        let prefix = "MY_HANDLER";
+
+        temp_env::with_vars(
+            vec![
+                (format!("{prefix}_AMPD_URL"), Some(ampd_url)),
+                (format!("{prefix}_CHAIN_NAME"), Some(chain_name)),
+            ],
+            || {
+                let config = Config::builder().add_env_source(prefix).build().unwrap();
+
+                assert_eq!(config.ampd_url, Url::new_sensitive(ampd_url).unwrap());
+                assert_eq!(config.chain_name, ChainName::from_str(chain_name).unwrap());
+            },
+        );
+    }
+
+    #[test]
+    fn config_loads_from_custom_file() {
+        let ampd_url = "http://localhost:8080";
+        let chain_name = "some-chain";
+
+        let temp_dir = TempDir::new().unwrap();
+        let config_path = temp_dir.path().join("custom-config.toml");
+        let content = format!(
+            r#"
+            ampd_url="{ampd_url}"
+            chain_name="{chain_name}"
+            "#
+        );
+        fs::write(&config_path, content).unwrap();
+
+        let config = Config::builder()
+            .add_file_source(config_path.to_str().unwrap(), true)
+            .build()
+            .unwrap();
+
+        assert_eq!(config.ampd_url, Url::new_sensitive(ampd_url).unwrap());
+        assert_eq!(config.chain_name, ChainName::from_str(chain_name).unwrap());
+    }
+
+    #[test]
+    fn config_load_fails_if_field_is_missing() {
+        let ampd_url = "http://localhost:8080";
+
+        let temp_dir = TempDir::new().unwrap();
+        let config_path = temp_dir.path().join("custom-config.toml");
+        let content = format!(
+            r#"
+            ampd_url="{ampd_url}"
+            "#
+        );
+        fs::write(&config_path, content).unwrap();
+
+        let res = Config::builder()
+            .add_file_source(config_path.to_str().unwrap(), true)
+            .build();
+
+        assert_err_contains!(res, Error, Error::Build);
     }
 }
