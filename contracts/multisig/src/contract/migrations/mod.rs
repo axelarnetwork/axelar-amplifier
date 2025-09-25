c46a5169e06691cdd877a4297f916b5d1dad7e27
SDavidson1177 <solomon@interoplabs.io>
2025-09-25 16:10:36 -0400

    multisig migration edge case multiple chains

--- contracts/multisig/src/contract/migrations/mod.rs
+++ contracts/multisig/src/contract/migrations/mod.rs
@@ -5,6 +5,7 @@ use cosmwasm_schema::cw_serde;
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{DepsMut, Env, Order, Response};
+use itertools::Itertools;
 
 use crate::contract::migrations::legacy_state::AUTHORIZED_CALLERS;
 use crate::state::{save_prover, Config, CONFIG};
@@ -45,6 +46,9 @@ fn migrate_authorized_callers(
     let authorized_callers: Vec<_> = AUTHORIZED_CALLERS
         .range(deps.storage, None, None, Order::Ascending)
         .filter_map(|value| value.ok())
+        .dedup_by(|&(_, ref chain_name_a), &(_, ref chain_name_b)| {
+            chain_name_a == chain_name_b
+        })
         .collect();
 
     for (contract_address, chain_name) in authorized_callers {
@@ -74,7 +78,8 @@ mod tests {
     const ADMIN: &str = "admin";
     const COORDINATOR: &str = "coordinator";
     const SENDER: &str = "sender";
-    const PROVER: &str = "prover";
+    const PROVER1: &str = "prover1";
+    const PROVER2: &str = "prover2";
 
     #[test]
     fn migrate_properly_registers_coordinator() {
@@ -134,7 +139,15 @@ mod tests {
         assert!(AUTHORIZED_CALLERS
             .save(
                 &mut deps.storage,
-                &cosmos_addr!(PROVER),
+                &cosmos_addr!(PROVER2),
+                &ChainName::from_str("chain1").unwrap()
+            )
+            .is_ok());
+
+        assert!(AUTHORIZED_CALLERS
+            .save(
+                &mut deps.storage,
+                &cosmos_addr!(PROVER1),
                 &ChainName::from_str("chain1").unwrap()
             )
             .is_ok());
@@ -143,17 +156,18 @@ mod tests {
         assert!(res.is_ok());
         assert_eq!(res.unwrap(), None);
 
-        assert!(migrate(
+        let res = migrate(
             deps.as_mut(),
             env,
             MigrateMsg {
                 coordinator: cosmos_addr!(COORDINATOR).to_string(),
             },
-        )
-        .is_ok());
+        );
+
+        assert!(res.is_ok());
 
         let res = prover_by_chain(&deps.storage, chain_name!("chain1"));
         assert!(res.is_ok());
-        assert_eq!(res.unwrap(), Some(cosmos_addr!(PROVER)));
+        assert_eq!(res.unwrap(), Some(cosmos_addr!(PROVER1)));
     }
 }
