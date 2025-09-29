a5aefe8dad7657ca5088c026b20a1f418a96b51d
SDavidson1177 <solomon@interoplabs.io>
2025-09-29 17:25:23 -0400

    reformat

--- contracts/multisig/src/contract/migrations/mod.rs
+++ contracts/multisig/src/contract/migrations/mod.rs
@@ -1,11 +1,13 @@
 mod legacy_state;
 
+use std::collections::{HashMap, HashSet};
+
 use axelar_wasm_std::{address, migrate_from_version};
 use cosmwasm_schema::cw_serde;
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{DepsMut, Env, Order, Response};
-use itertools::Itertools;
+use cosmwasm_std::{Addr, DepsMut, Env, Order, Response};
+use router_api::ChainName;
 
 use crate::contract::migrations::legacy_state::AUTHORIZED_CALLERS;
 use crate::state::{save_prover, Config, CONFIG};
@@ -13,6 +15,7 @@ use crate::state::{save_prover, Config, CONFIG};
 #[cw_serde]
 pub struct MigrateMsg {
     pub coordinator: String,
+    pub default_authorized_provers: HashMap<ChainName, Addr>,
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -35,22 +38,40 @@ pub fn migrate(
         },
     )?;
 
-    migrate_authorized_callers(&mut deps)?;
+    migrate_authorized_callers(&mut deps, msg.default_authorized_provers)?;
 
     Ok(Response::default())
 }
 
 fn migrate_authorized_callers(
     deps: &mut DepsMut,
+    authorized_provers: HashMap<ChainName, Addr>,
 ) -> Result<(), axelar_wasm_std::error::ContractError> {
-    let authorized_callers: Vec<_> = AUTHORIZED_CALLERS
-        .range(deps.storage, None, None, Order::Ascending)
-        .filter_map(|value| value.ok())
-        .dedup_by(|(_, chain_name_a), (_, chain_name_b)| chain_name_a == chain_name_b)
-        .collect();
+    let mut seen = HashMap::<ChainName, Addr>::new();
+    let mut duplicates = HashSet::<ChainName>::new();
 
-    for (contract_address, chain_name) in authorized_callers {
-        save_prover(deps.storage, contract_address, chain_name)?;
+    for result in AUTHORIZED_CALLERS
+        .range(deps.storage, None, None, Order::Ascending)
+        .filter_map(|result| result.ok())
+    {
+        let (prover_addr, chain_name) = result;
+
+        if seen.contains_key(&chain_name) {
+            duplicates.insert(chain_name.clone());
+        }
+        seen.insert(chain_name, prover_addr);
+    }
+
+    for (chain_name, prover_addr) in seen {
+        let addr = if duplicates.contains(&chain_name) {
+            authorized_provers
+                .get(&chain_name)
+                .cloned()
+                .unwrap_or(prover_addr)
+        } else {
+            prover_addr
+        };
+        save_prover(deps.storage, addr, chain_name)?;
     }
 
     Ok(())
@@ -58,6 +79,7 @@ fn migrate_authorized_callers(
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use std::str::FromStr;
 
     use axelar_wasm_std::address;
@@ -103,6 +125,7 @@ mod tests {
             env,
             MigrateMsg {
                 coordinator: cosmos_addr!(COORDINATOR).to_string(),
+                default_authorized_provers: HashMap::new(),
             },
         )
         .is_ok());
@@ -159,6 +182,64 @@ mod tests {
             env,
             MigrateMsg {
                 coordinator: cosmos_addr!(COORDINATOR).to_string(),
+                default_authorized_provers: HashMap::new(),
+            },
+        )
+        .is_ok());
+
+        let res = prover_by_chain(&deps.storage, chain_name!("chain1"));
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), Some(cosmos_addr!(PROVER2)));
+    }
+
+    #[test]
+    fn migrate_stores_authorized_callers_for_chains_using_default_provers() {
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let info = message_info(&cosmos_addr!(GOVERNANCE), &[]);
+
+        assert!(legacy_state::test::instantiate(
+            deps.as_mut(),
+            env.clone(),
+            info.clone(),
+            legacy_state::InstantiateMsg {
+                governance_address: cosmos_addr!(GOVERNANCE).to_string(),
+                admin_address: cosmos_addr!(ADMIN).to_string(),
+                rewards_address: cosmos_addr!(REWARDS).to_string(),
+                block_expiry: Uint64::try_from(100).unwrap(),
+            },
+        )
+        .is_ok());
+
+        assert!(AUTHORIZED_CALLERS
+            .save(
+                &mut deps.storage,
+                &cosmos_addr!(PROVER2),
+                &ChainName::from_str("chain1").unwrap()
+            )
+            .is_ok());
+
+        assert!(AUTHORIZED_CALLERS
+            .save(
+                &mut deps.storage,
+                &cosmos_addr!(PROVER1),
+                &ChainName::from_str("chain1").unwrap()
+            )
+            .is_ok());
+
+        let res = prover_by_chain(&deps.storage, chain_name!("chain1"));
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), None);
+
+        assert!(migrate(
+            deps.as_mut(),
+            env,
+            MigrateMsg {
+                coordinator: cosmos_addr!(COORDINATOR).to_string(),
+                default_authorized_provers: HashMap::from([(
+                    chain_name!("chain1"),
+                    cosmos_addr!(PROVER1)
+                )]),
             },
         )
         .is_ok());
