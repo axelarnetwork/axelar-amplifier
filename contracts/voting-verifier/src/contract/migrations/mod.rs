96735c75e584fc5118e450426d358be0d08b3bc2
Christian Gorenflo <christian@interoplabs.io>
2025-11-18 16:32:31 +0100

    allow migration from any version

--- contracts/voting-verifier/src/contract/migrations/mod.rs
+++ contracts/voting-verifier/src/contract/migrations/mod.rs
@@ -5,12 +5,21 @@ use cosmwasm_std::{DepsMut, Empty, Env, Response};
 
 pub type MigrateMsg = Empty;
 
+const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
 #[cfg_attr(not(feature = "library"), entry_point)]
-#[migrate_from_version("1.1")]
+// The migrate version check attribute is commented out and left here on purpose as a reminder that
+// this should generally not be removed. However, for this migration, we need to upgrade both from
+// 1.0 and 1.1 and there are no state migrations from either version, so the impact of this missing
+// check is low. The next migration after that should make use of the version check again
+//#[migrate_from_version("1.1")]
 pub fn migrate(
     deps: DepsMut,
     _env: Env,
     _msg: MigrateMsg,
 ) -> Result<Response, axelar_wasm_std::error::ContractError> {
+    cw2::set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
     Ok(Response::default())
 }
