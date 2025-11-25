ac77e837cc328beed24bef9bf2a8187963f79459
CJ Cobb <cj@interoplabs.io>
2025-11-25 15:34:50 -0500

    improve error message

--- ampd-handlers/src/bin/evm/error.rs
+++ ampd-handlers/src/bin/evm/error.rs
@@ -6,7 +6,7 @@ pub enum Error {
     HandlerStart,
     #[error("event verifier contract not found in contracts response")]
     EventVerifierContractNotFound,
-    #[error("missing confirmation height")]
+    #[error("missing confirmation height in config. event verification handler requires confirmation height when finalization is ConfirmationHeight")]
     MissingConfirmationHeight,
     #[error("handler task failed")]
     HandlerTask,
