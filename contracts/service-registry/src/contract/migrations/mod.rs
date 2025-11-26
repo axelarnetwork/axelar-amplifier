2ff8e98cadb27a8049f855f39b8ba2bcf227d40b
SDavidson1177 <solomon@interoplabs.io>
2025-11-26 17:34:46 -0500

    add a way to update the authorized verifier cound

--- contracts/service-registry/src/contract/migrations/mod.rs
+++ contracts/service-registry/src/contract/migrations/mod.rs
@@ -1,16 +1,35 @@
-use axelar_wasm_std::migrate_from_version;
+use std::usize;
+
+use axelar_wasm_std::{migrate_from_version, nonempty::Usize, IntoContractError};
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{DepsMut, Empty, Env, Response};
+use error_stack::ResultExt;
+use service_registry_api::{error::ContractError};
+use crate::state::{services, update_authorized_verifier_count};
 
 pub type MigrateMsg = Empty;
 
+#[derive(thiserror::Error, Debug, PartialEq, IntoContractError)]
+enum MigrationError {
+    #[error("invalid limit")]
+    InvalidLimit,
+}
+
 #[cfg_attr(not(feature = "library"), entry_point)]
-#[migrate_from_version("1.1")]
+#[migrate_from_version("1.2")]
 pub fn migrate(
     deps: DepsMut,
     _env: Env,
     _msg: MigrateMsg,
 ) -> Result<Response, axelar_wasm_std::error::ContractError> {
+    let services = services(deps.storage, None, Usize::try_from(usize::MAX)
+        .change_context(MigrationError::InvalidLimit)?)
+        .change_context(ContractError::ServiceNotFound)?;
+
+    for s in services {
+        update_authorized_verifier_count(deps.storage, &s.name)?;
+    }
+    
     Ok(Response::default())
 }
