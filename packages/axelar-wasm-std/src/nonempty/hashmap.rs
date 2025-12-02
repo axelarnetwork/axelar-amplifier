3d6865e45f0fcac0478ea2f6326694d8848a406c
Christian Gorenflo <christian@interoplabs.io>
2025-12-02 23:24:35 +0100

    derive traits

--- packages/axelar-wasm-std/src/nonempty/hashmap.rs
+++ packages/axelar-wasm-std/src/nonempty/hashmap.rs
@@ -5,18 +5,10 @@ use serde::Serialize;
 
 use crate::nonempty::Error;
 
-#[derive(Clone, Debug, Serialize)]
-pub struct HashMap<K, V>(std::collections::HashMap<K, V>);
+#[derive(Clone, Debug, Serialize, PartialEq, Eq)]
+pub struct HashMap<K: Eq + Hash, V: Eq>(std::collections::HashMap<K, V>);
 
-impl<K: Eq + Hash, V: PartialEq> PartialEq for HashMap<K, V> {
-    fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0
-    }
-}
-
-impl<K: Eq + Hash, V: Eq> Eq for HashMap<K, V> {}
-
-impl<K: Eq + Hash, V> TryFrom<std::collections::HashMap<K, V>> for HashMap<K, V> {
+impl<K: Eq + Hash, V: Eq> TryFrom<std::collections::HashMap<K, V>> for HashMap<K, V> {
     type Error = Error;
 
     fn try_from(value: std::collections::HashMap<K, V>) -> Result<Self, Self::Error> {
@@ -28,19 +20,19 @@ impl<K: Eq + Hash, V> TryFrom<std::collections::HashMap<K, V>> for HashMap<K, V>
     }
 }
 
-impl<K, V> From<HashMap<K, V>> for std::collections::HashMap<K, V> {
+impl<K: Eq + Hash, V: Eq> From<HashMap<K, V>> for std::collections::HashMap<K, V> {
     fn from(value: HashMap<K, V>) -> Self {
         value.0
     }
 }
 
-impl<K, V> AsRef<std::collections::HashMap<K, V>> for HashMap<K, V> {
+impl<K: Eq + Hash, V: Eq> AsRef<std::collections::HashMap<K, V>> for HashMap<K, V> {
     fn as_ref(&self) -> &std::collections::HashMap<K, V> {
         &self.0
     }
 }
 
-impl<K, V> Deref for HashMap<K, V> {
+impl<K: Eq + Hash, V: Eq> Deref for HashMap<K, V> {
     type Target = std::collections::HashMap<K, V>;
 
     fn deref(&self) -> &Self::Target {
