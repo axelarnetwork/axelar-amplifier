c791b88c324920f26990d98d7c43f177b6734f7c
Drew Taylor <drew@chainofinsight.com>
2025-12-12 15:25:33 -0500

    add test -> should_record_message_verification_vote_metric

--- ampd-handlers/src/bin/solana/handler.rs
+++ ampd-handlers/src/bin/solana/handler.rs
@@ -275,7 +275,7 @@ mod tests {
     use std::str::FromStr;
 
     use ampd::handlers::test_utils::{into_structured_event, participants};
-    use ampd::monitoring::{test_utils};
+    use ampd::monitoring::{metrics, test_utils};
     use ampd::solana::Client;
     use ampd::types::{Hash, TMAddress};
     use ampd_sdk::grpc::client::test_utils::MockHandlerTaskClient;
@@ -293,6 +293,7 @@ mod tests {
 
     use super::{
         Handler, EventHandler, PollStartedEvent, Pubkey,
+        Signature, SolanaRpcClientProxy, SolanaTransaction, Vote,
     };
 
     const PREFIX: &str = "axelar";
@@ -381,6 +382,23 @@ mod tests {
                     .unwrap(),
                 verifier_set: build_verifier_set(KeyType::Ecdsa, &ecdsa_test_data::signers()),
             },
+        }
+    }
+
+    struct ValidResponseSolanaRpc;
+    #[async_trait::async_trait]
+    impl SolanaRpcClientProxy for ValidResponseSolanaRpc {
+        async fn tx(&self, signature: &Signature) -> Option<SolanaTransaction> {
+            Some(SolanaTransaction {
+                signature: *signature,
+                inner_instructions: vec![],
+                err: None,
+                account_keys: vec![axelar_solana_gateway::ID], // Gateway program at index 0
+            })
+        }
+
+        async fn domain_separator(&self, _gateway_address: &Pubkey) -> Option<[u8; 32]> {
+            unimplemented!()
         }
     }
 
@@ -529,17 +547,11 @@ mod tests {
 
         let (monitoring_client, _) = test_utils::monitoring_client();
 
-        let rpc_client = Client::new(
-            mock_rpc_client(),
-            monitoring_client.clone(),
-            chain_name!("solana"),
-        );
-
         let handler = Handler::builder()
             .verifier(verifier.into())
             .voting_verifier_contract(voting_verifier.into())
             .chain(chain_name!("solana"))
-            .rpc_client(rpc_client)
+            .rpc_client(ValidResponseSolanaRpc)
             .gateway_address(gateway_address)
             .domain_separator(domain_separator)
             .monitoring_client(monitoring_client)
@@ -554,5 +566,51 @@ mod tests {
 
         assert_eq!(res.len(), 1);
         assert!(MsgExecuteContract::from_any(res.first().unwrap()).is_ok());
+    }
+
+    #[async_test]
+    async fn should_record_message_verification_vote_metric() {
+        let solana_chain_name = chain_name!("solana");
+        let gateway_address = axelar_solana_gateway::ID;
+        let domain_separator: [u8; 32] = [42; 32];
+        let voting_verifier = TMAddress::random(PREFIX);
+        let verifier = TMAddress::random(PREFIX);
+        let expiration = 100u64;
+
+        let event = into_structured_event(
+            message_poll_started_event(participants(5, Some(verifier.clone())), expiration),
+            &voting_verifier,
+        );
+
+        let (monitoring_client, mut receiver) = test_utils::monitoring_client();
+
+        let handler = Handler::builder()
+            .verifier(verifier.into())
+            .voting_verifier_contract(voting_verifier.into())
+            .chain(solana_chain_name.clone())
+            .rpc_client(ValidResponseSolanaRpc)
+            .gateway_address(gateway_address)
+            .domain_separator(domain_separator)
+            .monitoring_client(monitoring_client)
+            .build();
+
+        let mut client = mock_handler_client(expiration - 1);
+
+        let res = handler
+            .handle(event.try_into().unwrap(), &mut client)
+            .await;
+        assert!(res.is_ok());
+
+        for _ in 0..2 {
+            let msg = receiver.recv().await.unwrap();
+            assert_eq!(
+                msg,
+                metrics::Msg::VerificationVote {
+                    vote_decision: Vote::NotFound,
+                    chain_name: solana_chain_name.clone(),
+                }
+            );
+        }
+        assert!(receiver.try_recv().is_err());
     }
 }
