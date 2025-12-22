13eac03f275d74c4e851feae88e3fdc412fdd583
chipshort <christoph@interoplabs.io>
2025-12-22 18:24:33 +0100

    chore: remove voting-verifier and multisig-prover migrations

--- contracts/multisig-prover/src/contract/migrations/mod.rs
+++ contracts/multisig-prover/src/contract/migrations/mod.rs
@@ -1,15 +1,8 @@
-use axelar_wasm_std::address::validate_cosmwasm_address;
-use axelar_wasm_std::hash::Hash;
-use axelar_wasm_std::{migrate_from_version, MajorityThreshold};
+use axelar_wasm_std::migrate_from_version;
 use cosmwasm_schema::cw_serde;
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{Addr, DepsMut, Env, Response};
-use cw_storage_plus::Item;
-use multisig::key::KeyType;
-use router_api::ChainName;
-
-use crate::state::{Config, CONFIG};
+use cosmwasm_std::{DepsMut, Env, Response};
 
 #[cw_serde]
 pub struct MigrateMsg {
@@ -18,60 +11,11 @@ pub struct MigrateMsg {
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-#[migrate_from_version("1.1")]
+#[migrate_from_version("1.2")]
 pub fn migrate(
-    deps: DepsMut,
+    _deps: DepsMut,
     _env: Env,
-    msg: MigrateMsg,
+    _msg: MigrateMsg,
 ) -> Result<Response, axelar_wasm_std::error::ContractError> {
-    let chain_codec_address = validate_cosmwasm_address(deps.api, &msg.chain_codec_address)?;
-
-    let old_config = OLD_CONFIG.load(deps.storage)?;
-
-    CONFIG.save(
-        deps.storage,
-        &Config {
-            gateway: old_config.gateway,
-            multisig: old_config.multisig,
-            coordinator: old_config.coordinator,
-            service_registry: old_config.service_registry,
-            voting_verifier: old_config.voting_verifier,
-            signing_threshold: old_config.signing_threshold,
-            service_name: old_config.service_name,
-            chain_name: old_config.chain_name,
-            verifier_set_diff_threshold: old_config.verifier_set_diff_threshold,
-            key_type: old_config.key_type,
-            domain_separator: old_config.domain_separator,
-            chain_codec: chain_codec_address,
-
-            // existing deployments with our own multisig-prover contract don't need to receive full message payloads, so we don't enable it here
-            expect_full_message_payloads: false,
-            // existing deployments with our own multisig-prover contract don't use notify signing session, so we don't enable it here
-            // we might have to enable it if we migrate other multisig-prover forks in the future
-            notify_signing_session: false,
-            // existing deployments couldn't use a custom signature verifier, so we don't have to specify one here
-            sig_verifier_address: None,
-        },
-    )?;
     Ok(Response::default())
-}
-
-pub const OLD_CONFIG: Item<OldConfig> = Item::new("config");
-
-#[cw_serde]
-pub struct OldConfig {
-    pub gateway: Addr,
-    pub multisig: Addr,
-    pub coordinator: Addr,
-    pub service_registry: Addr,
-    pub voting_verifier: Addr,
-    pub signing_threshold: MajorityThreshold,
-    pub service_name: String,
-    pub chain_name: ChainName,
-    pub verifier_set_diff_threshold: u32,
-    // we are dropping this with the migration anyway and the type does not exist anymore,
-    // so we just comment it out
-    // pub encoder: Encoder,
-    pub domain_separator: Hash,
-    pub key_type: KeyType,
 }
--- contracts/voting-verifier/src/contract/migrations/mod.rs
+++ contracts/voting-verifier/src/contract/migrations/mod.rs
@@ -1,14 +1,6 @@
-use axelar_wasm_std::address::{validate_cosmwasm_address, AddressFormat};
-use axelar_wasm_std::msg_id::MessageIdFormat;
-use axelar_wasm_std::{migrate_from_version, nonempty, MajorityThreshold};
+use axelar_wasm_std::migrate_from_version;
 use cosmwasm_schema::cw_serde;
-#[cfg(not(feature = "library"))]
-use cosmwasm_std::entry_point;
-use cosmwasm_std::{Addr, DepsMut, Env, Response};
-use cw_storage_plus::Item;
-use router_api::ChainName;
-
-use crate::state::{Config, CONFIG};
+use cosmwasm_std::{DepsMut, Env, Response};
 
 #[cw_serde]
 pub struct MigrateMsg {
@@ -19,49 +11,9 @@ pub struct MigrateMsg {
 #[cfg_attr(not(feature = "library"), entry_point)]
 #[migrate_from_version("2.0")]
 pub fn migrate(
-    deps: DepsMut,
+    _deps: DepsMut,
     _env: Env,
-    msg: MigrateMsg,
+    _msg: MigrateMsg,
 ) -> Result<Response, axelar_wasm_std::error::ContractError> {
-    let chain_codec_address = validate_cosmwasm_address(deps.api, &msg.chain_codec_address)?;
-
-    let old_config = OLD_CONFIG.load(deps.storage)?;
-
-    // query chain codec with source gateway address as a sanity check
-    // if it fails, the chain codec contract is incorrect
-    let chain_codec: chain_codec_api::Client =
-        client::ContractClient::new(deps.querier, &chain_codec_address).into();
-    chain_codec.validate_address(old_config.source_gateway_address.to_string())?;
-
-    CONFIG.save(
-        deps.storage,
-        &Config {
-            service_registry_contract: old_config.service_registry_contract,
-            service_name: old_config.service_name,
-            source_gateway_address: old_config.source_gateway_address,
-            voting_threshold: old_config.voting_threshold,
-            block_expiry: old_config.block_expiry,
-            confirmation_height: old_config.confirmation_height,
-            source_chain: old_config.source_chain,
-            rewards_contract: old_config.rewards_contract,
-            msg_id_format: old_config.msg_id_format,
-            chain_codec_address,
-        },
-    )?;
     Ok(Response::default())
-}
-
-pub const OLD_CONFIG: Item<OldConfig> = Item::new("config");
-#[cw_serde]
-pub struct OldConfig {
-    pub service_registry_contract: Addr,
-    pub service_name: nonempty::String,
-    pub source_gateway_address: nonempty::String,
-    pub voting_threshold: MajorityThreshold,
-    pub block_expiry: nonempty::Uint64, // number of blocks after which a poll expires
-    pub confirmation_height: u64,
-    pub source_chain: ChainName,
-    pub rewards_contract: Addr,
-    pub msg_id_format: MessageIdFormat,
-    pub address_format: AddressFormat,
 }
