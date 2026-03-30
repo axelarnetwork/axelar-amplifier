d2fe5782ba2a5f685b8d649833a9e2a3c25b5ff0
Nikola Ristić <ristic@risticnikola.com>
2026-03-30 22:07:54 +0200

    feat: allow updates of chain_codec_address in mp

--- contracts/multisig-prover/src/contract/migrations/mod.rs
+++ contracts/multisig-prover/src/contract/migrations/mod.rs
@@ -1,21 +1,32 @@
-use axelar_wasm_std::migrate_from_version;
+use axelar_wasm_std::{address, migrate_from_version};
 use cosmwasm_schema::cw_serde;
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{DepsMut, Env, Response};
 
+use crate::state::CONFIG;
+
 #[cw_serde]
 pub struct MigrateMsg {
-    /// Address to the chain codec contract to use for this migration
-    pub chain_codec_address: String,
+    /// Optional new address for the chain codec contract
+    pub chain_codec_address: Option<String>,
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 #[migrate_from_version("1.2")]
 pub fn migrate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
-    _msg: MigrateMsg,
+    msg: MigrateMsg,
 ) -> Result<Response, axelar_wasm_std::error::ContractError> {
+    if let Some(chain_codec_address) = msg.chain_codec_address {
+        let chain_codec = address::validate_cosmwasm_address(deps.api, &chain_codec_address)?;
+
+        CONFIG.update(deps.storage, |mut config| -> Result<_, axelar_wasm_std::error::ContractError> {
+            config.chain_codec = chain_codec;
+            Ok(config)
+        })?;
+    }
+
     Ok(Response::default())
 }
