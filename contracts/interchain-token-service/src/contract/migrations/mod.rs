502893c5965aff2ac9fb70f21ea7c4898f1524d3
themicp <themicp@gmail.com>
2026-04-09 14:06:44 +0000

    test(its): improve migration test to verify other token instances are preserved

--- contracts/interchain-token-service/src/contract/migrations/mod.rs
+++ contracts/interchain-token-service/src/contract/migrations/mod.rs
@@ -51,8 +51,11 @@ mod tests {
     fn migration_removes_stellar_xrp_token_instance() {
         let mut deps = mock_dependencies();
 
-        let chain: ChainNameRaw = "stellar".parse().unwrap();
-        let token_id = TokenId::new(
+        let stellar: ChainNameRaw = "stellar".parse().unwrap();
+        let xrpl: ChainNameRaw = "xrpl".parse().unwrap();
+        let ethereum: ChainNameRaw = "ethereum".parse().unwrap();
+
+        let xrp_token_id = TokenId::new(
             <[u8; 32]>::try_from(
                 cosmwasm_std::HexBinary::from_hex(
                     "ba5a21ca88ef6bba2bfff5088994f90e1077e2a1cc3dcc38bd261f00fce2824f",
@@ -62,27 +65,64 @@ mod tests {
             )
             .unwrap(),
         );
+        let other_token_id = TokenId::new([1u8; 32]);
 
-        // set up the token instance that should be removed
+        let untracked_instance = TokenInstance {
+            supply: TokenSupply::Untracked,
+            decimals: 6,
+        };
+
+        // the target: XRP on stellar
         state::save_token_instance(
             deps.as_mut().storage,
-            chain.clone(),
-            token_id,
-            &TokenInstance {
-                supply: TokenSupply::Untracked,
-                decimals: 6,
-            },
+            stellar.clone(),
+            xrp_token_id,
+            &untracked_instance,
         )
         .unwrap();
 
-        assert!(state::may_load_token_instance(deps.as_ref().storage, chain.clone(), token_id)
-            .unwrap()
-            .is_some());
+        // XRP on xrpl (should be kept)
+        state::save_token_instance(
+            deps.as_mut().storage,
+            xrpl.clone(),
+            xrp_token_id,
+            &untracked_instance,
+        )
+        .unwrap();
+
+        // different token on stellar (should be kept)
+        state::save_token_instance(
+            deps.as_mut().storage,
+            stellar.clone(),
+            other_token_id,
+            &untracked_instance,
+        )
+        .unwrap();
+
+        // XRP on ethereum (should be kept)
+        state::save_token_instance(
+            deps.as_mut().storage,
+            ethereum.clone(),
+            xrp_token_id,
+            &untracked_instance,
+        )
+        .unwrap();
 
         remove_stellar_xrp_token_instance(deps.as_mut().storage);
 
-        assert!(state::may_load_token_instance(deps.as_ref().storage, chain, token_id)
+        // only stellar + xrp_token_id should be removed
+        assert!(state::may_load_token_instance(deps.as_ref().storage, stellar.clone(), xrp_token_id)
             .unwrap()
             .is_none());
+
+        assert!(state::may_load_token_instance(deps.as_ref().storage, xrpl, xrp_token_id)
+            .unwrap()
+            .is_some());
+        assert!(state::may_load_token_instance(deps.as_ref().storage, stellar, other_token_id)
+            .unwrap()
+            .is_some());
+        assert!(state::may_load_token_instance(deps.as_ref().storage, ethereum, xrp_token_id)
+            .unwrap()
+            .is_some());
     }
 }
